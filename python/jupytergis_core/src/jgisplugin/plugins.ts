576b7b88326487208e72d5524071b3145e6d1ef1
martinRenou <martin.renou@gmail.com>
2024-07-10 12:41:40 +0200

    Missing default values for position

--- python/jupytergis_core/src/jgisplugin/plugins.ts
+++ python/jupytergis_core/src/jgisplugin/plugins.ts
@@ -111,7 +111,7 @@ const activate = (
         format: 'text',
         size: undefined,
         content:
-          '{\n\t"layers": {},\n\t"sources": {},\n\t"options": {},\n\t"layerTree": []\n}'
+          '{\n\t"layers": {},\n\t"sources": {},\n\t"options": {"latitude": 0, "longitude": 0, "zoom": 0},\n\t"layerTree": []\n}'
       });
 
       // Open the newly created file with the 'Editor'
