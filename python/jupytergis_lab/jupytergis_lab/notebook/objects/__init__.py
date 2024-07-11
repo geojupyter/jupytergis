94f0adc64a2669ea9942579ac157c77ab0ab646f
martinRenou <martin.renou@gmail.com>
2024-07-11 13:00:49 +0200

    Notebook API

--- python/jupytergis_lab/jupytergis_lab/notebook/objects/__init__.py
+++ python/jupytergis_lab/jupytergis_lab/notebook/objects/__init__.py
@@ -1,1 +1,7 @@
 from ._schema.jgis import *  # noqa
+
+from ._schema.rasterlayer import IRasterLayer  # noqa
+from ._schema.vectorLayer import IVectorLayer  # noqa
+
+from ._schema.rastersource import IRasterSource  # noqa
+from ._schema.geojsonsource import IGeoJSONSource  # noqa
