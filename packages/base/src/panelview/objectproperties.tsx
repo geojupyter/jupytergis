cc6e33426a6d64084af710eccea530c030536dee
martinRenou <martin.renou@gmail.com>
2024-07-19 17:03:23 +0200

    Remove logs
    Co-authored-by: Nicolas Brichet <32258950+brichet@users.noreply.github.com>

--- packages/base/src/panelview/objectproperties.tsx
+++ packages/base/src/panelview/objectproperties.tsx
@@ -132,11 +132,6 @@ class ObjectPropertiesReact extends React.Component<IProps, IStates> {
   render(): React.ReactNode {
     const selectedObject = this.state.selectedObject;
 
-    console.log(
-      'selected changed!',
-      selectedObject,
-      this.state.context?.localPath
-    );
 
     if (!selectedObject || !this.state.context) {
       return <div></div>;
