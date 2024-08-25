c189266c3661a9572d378ea9917d8fcf1e6be6c9
arjxn.py <arjxn.py@gmail.com>
2024-08-25 16:41:43 +0530

    Add remaining function (Still No Success)

--- python/jupytergis_core/jupytergis_core/__init__.py
+++ python/jupytergis_core/jupytergis_core/__init__.py
@@ -14,6 +14,9 @@ from .handler import setup_handlers
 def _jupyter_labextension_paths():
     return [{"src": "labextension", "dest": "@jupytergis/jupytergis-core"}]
 
+def _jupyter_server_extension_points():
+    return [{"module": "jupytergis_core"}]
+
 def _load_jupyter_server_extension(server_app):
     """Registers the API handler to receive HTTP requests from the frontend extension.
     
