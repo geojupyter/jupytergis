30d49fde4805faccba75ba10c4b8bbedf1da530b
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-08-28 10:40:49 +0200

    lint

--- ui-tests/tests/ui.spec.ts
+++ ui-tests/tests/ui.spec.ts
@@ -19,7 +19,7 @@ test.describe('UI Test', () => {
     test.beforeEach(async ({ page }) => {
       const unrelatedErrors = [
         // This error is related to plotly dependency, installed with qgis.
-        '@jupyter-widgets/base doesn\'t exist in shared scope default'
+        "@jupyter-widgets/base doesn't exist in shared scope default"
       ];
       page.setViewportSize({ width: 1920, height: 1080 });
       page.on('console', message => {
