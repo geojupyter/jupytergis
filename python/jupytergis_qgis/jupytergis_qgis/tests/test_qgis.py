aa6d53b60e8a782d9f98a9e6cca561b1c85c1f16
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-09-09 12:45:33 +0200

    Add test on file saving

--- python/jupytergis_qgis/jupytergis_qgis/tests/test_qgis.py
+++ python/jupytergis_qgis/jupytergis_qgis/tests/test_qgis.py
@@ -1,8 +1,11 @@
+import os
+import sys
 from pathlib import Path
-
+from uuid import uuid4
 from dirty_equals import IsPartialDict, IsStr
+from qgis.core import QgsApplication
 
-from ..qgis_loader import import_project_from_qgis
+from ..qgis_loader import import_project_from_qgis, export_project_to_qgis
 
 
 FILES = Path(__file__).parent / "files"
@@ -14,7 +17,6 @@ def test_qgis_loader():
     source_id1 = IsStr()
     source_id2 = IsStr()
     source_id3 = IsStr()
-    print(jgis)
     assert jgis == IsPartialDict(
         options={
             "bearing": 0,
@@ -112,3 +114,118 @@ def test_qgis_loader():
             },
         },
     )
+
+
+def test_qgis_saver():
+    filename = FILES / "project1.qgz"
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    layer_ids = [str(uuid4()), str(uuid4()), str(uuid4()), str(uuid4())]
+    source_ids = [str(uuid4()), str(uuid4()), str(uuid4()), str(uuid4())]
+    jgis = {
+        "options": {
+            "bearing": 0,
+            "pitch": 0,
+            'extent': [
+                -25164292.70393259,
+                -15184674.291019961,
+                26220958.18294687,
+                20663680.478501424
+            ]
+        },
+        "layers": {
+            layer_ids[0]: {
+                "name": "OpenStreetMap0",
+                "parameters": {
+                    "source": source_ids[0],
+                },
+                "type": "RasterLayer",
+                "visible": True,
+            },
+            layer_ids[1]: {
+                "name": "OpenStreetMap1",
+                "parameters": {
+                    "source": source_ids[1],
+                },
+                "type": "RasterLayer",
+                "visible": True,
+            },
+            layer_ids[2]: {
+                "name": "OpenStreetMap2",
+                "parameters": {
+                    "source": source_ids[2],
+                },
+                "type": "RasterLayer",
+                "visible": True,
+            },
+            layer_ids[3]: {
+                "name": "OpenStreetMap3",
+                "parameters": {
+                    "source": source_ids[3],
+                },
+                "type": "RasterLayer",
+                "visible": True,
+            },
+        },
+        "layerTree": [
+            layer_ids[0],
+            layer_ids[1],
+            {
+                "layers": [
+                    layer_ids[2],
+                    layer_ids[3],
+                ],
+                "name": "group0",
+            },
+        ],
+        "sources": {
+            source_ids[0]: {
+                "name": "OpenStreetMap0 Source",
+                "type": "RasterSource",
+                "parameters": {
+                    "url": "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    "maxZoom": 19,
+                    "minZoom": 0,
+                },
+            },
+            source_ids[1]: {
+                "name": "OpenStreetMap1 Source",
+                "type": "RasterSource",
+                "parameters": {
+                    "url": "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    "maxZoom": 19,
+                    "minZoom": 0,
+                },
+            },
+            source_ids[2]: {
+                "name": "OpenStreetMap2 Source",
+                "type": "RasterSource",
+                "parameters": {
+                    "url": "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    "maxZoom": 19,
+                    "minZoom": 0,
+                },
+            },
+            source_ids[3]: {
+                "name": "OpenStreetMap3 Source",
+                "type": "RasterSource",
+                "parameters": {
+                    "url": "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    "maxZoom": 19,
+                    "minZoom": 0,
+                },
+            },
+        }
+    }
+
+    QgsApplication.setPrefixPath(sys.prefix)
+
+    qgs = QgsApplication([], False)
+    qgs.initQgis()
+    assert export_project_to_qgis(filename, jgis)
+    qgs.exitQgis()
+
+    imported_jgis = import_project_from_qgis(filename)
+
+    assert jgis == imported_jgis
