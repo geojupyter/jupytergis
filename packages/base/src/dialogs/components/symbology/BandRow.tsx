1f6429a279d42b572620b1acd8e0f4cb6f652843
Greg <gjmooney@asu.edu>
2024-09-11 11:35:53 +0200

    Css

--- packages/base/src/dialogs/components/symbology/BandRow.tsx
+++ packages/base/src/dialogs/components/symbology/BandRow.tsx
@@ -13,29 +13,65 @@ const BandRow = ({
   setSelectedBand: any;
 }) => {
   return (
-    <div className="jp-gis-band-row-container">
+    <>
       <div className="jp-gis-symbology-row">
         <label htmlFor={`band-select-${index}`}>Band:</label>
+        <div className="jp-select-wrapper">
+          <select
+            name={`band-select-${index}`}
+            onChange={event => setSelectedBand(event.target.value)}
+            className="jp-mod-styled"
+          >
+            {bandRows.map((band, bandIndex) => (
+              <option
+                key={bandIndex}
+                value={band.band}
+                selected={band.band === bandRow.band}
+                className="jp-mod-styled"
+              >
+                {`Band ${band.band} (${band.colorInterpretation})`}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div className="jp-select-wrapper">
-        <select
-          name={`band-select-${index}`}
-          onChange={event => setSelectedBand(event.target.value)}
-          className="jp-mod-styled"
+      <div className="jp-gis-symbology-row" style={{ gap: '0.5rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '50%'
+          }}
         >
-          {bandRows.map((band, bandIndex) => (
-            <option
-              key={bandIndex}
-              value={band.band}
-              selected={band.band === bandRow.band}
-              className="jp-mod-styled"
-            >
-              {`Band ${band.band} (${band.colorInterpretation})`}
-            </option>
-          ))}
-        </select>
+          <label htmlFor="band-min" style={{ alignSelf: 'center' }}>
+            Min
+          </label>
+          <input
+            type="number"
+            defaultValue={bandRow.stats.minimum}
+            className="jp-mod-styled"
+            style={{ marginRight: 15 }}
+          />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '50%',
+            paddingRight: '2px'
+          }}
+        >
+          <label htmlFor="band-max" style={{ alignSelf: 'center' }}>
+            Max
+          </label>
+          <input
+            type="number"
+            defaultValue={bandRow.stats.maximum}
+            className="jp-mod-styled"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
