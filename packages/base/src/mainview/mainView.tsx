f19a2c6b4969e0c6a7ef4ef8301f493303f464a9
arjxn-py <arjxn.py@gmail.com>
2024-09-17 03:24:27 +0530

    Add handling for line type in vector layer

--- packages/base/src/mainview/mainView.tsx
+++ packages/base/src/mainview/mainView.tsx
@@ -647,6 +647,8 @@ export class MainView extends React.Component<IProps, IStates> {
       color:
         layerParameters.type === 'fill' || layerParameters.type === 'circle'
           ? layerParameters.color
+          : layerParameters.type === 'line'
+          ? 'rgba(0, 0, 0, 0)'
           : '#F092DD'
     });
 
