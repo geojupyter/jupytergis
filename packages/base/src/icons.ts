0af99a6c227c46684c72ed96665d137e4f363867
martinRenou <martin.renou@gmail.com>
2024-09-17 09:22:31 +0100

    Add icons (#144)
    * Add icons
    
    * Add more icon
    
    * Update Playwright Snapshots
    
    ---------
    
    Co-authored-by: Isabel Paredes <isabel.paredes@quantstack.net>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/base/src/icons.ts
+++ packages/base/src/icons.ts
@@ -7,11 +7,29 @@
 
 import { LabIcon } from '@jupyterlab/ui-components';
 
+import logoSvgStr from '../style/icons/logo.svg';
+import logoMiniSvgStr from '../style/icons/logo_mini.svg';
+import logoMiniAlternativeSvgStr from '../style/icons/logo_mini_alternative.svg';
 import rasterSvgStr from '../style/icons/raster.svg';
 import visibilitySvgStr from '../style/icons/visibility.svg';
 import nonVisibilitySvgStr from '../style/icons/nonvisibility.svg';
 import geoJsonSvgStr from '../style/icons/geojson.svg';
 import moundSvgStr from '../style/icons/mound.svg';
+
+export const logoIcon = new LabIcon({
+  name: 'jupytergis::logo',
+  svgstr: logoSvgStr
+});
+
+export const logoMiniIcon = new LabIcon({
+  name: 'jupytergis::logoMini',
+  svgstr: logoMiniSvgStr
+});
+
+export const logoMiniAlternativeIcon = new LabIcon({
+  name: 'jupytergis::logoMiniAlternative',
+  svgstr: logoMiniAlternativeSvgStr
+});
 
 export const rasterIcon = new LabIcon({
   name: 'jupytergis::raster',
