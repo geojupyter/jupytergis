ecd5828ee30a8bf01fe0279e005af8be74fca48c
martinRenou <martin.renou@gmail.com>
2024-09-23 14:40:57 +0200

    Improve gdal initialization
        - Fix BaseUrl issue with lab/lite
        - Load gdal at startup and only once

---
+++ packages/base/src/gdal.ts
@@ -0,0 +1,18 @@
+import initGdalJs from 'gdal3.js';
+
+export async function getGdal() {
+  const dataurl = new URL('./gdal3WebAssembly.data', import.meta.url);
+  const wasmurl = new URL('./gdal3WebAssembly.wasm', import.meta.url);
+
+  // TODO Pass gdal JS too and run gdal in a worker?
+  return await initGdalJs({
+    paths: {
+      wasm: wasmurl.href,
+      data: dataurl.href
+    },
+    useWorker: false
+  });
+}
+
+// Early load gdal
+getGdal();
