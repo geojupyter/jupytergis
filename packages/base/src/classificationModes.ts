cae89834f805d4e6468dd7b429ed95b4305997c1
Greg <gjmooney@asu.edu>
2024-10-09 11:47:54 +0200

    Save

--- packages/base/src/classificationModes.ts
+++ packages/base/src/classificationModes.ts
@@ -62,3 +62,91 @@ export const calculateEqualIntervalBreaks = (
 
   return breaks;
 };
+
+export const calculateJenksBreaks = (
+  values: number[],
+  numOfClasses: number
+) => {
+  // const minimum = Math.min(...values);
+  const maximum = Math.max(...values);
+
+  if (values.length === 0) {
+    return [];
+  }
+
+  if (numOfClasses <= 1) {
+    return [maximum];
+  }
+
+  if (numOfClasses >= values.length) {
+    return values;
+  }
+
+  // skipping the lots of values part
+  // const sample = values;
+  // let sorted = [];
+
+  const sample = [...values].sort((a, b) => a - b);
+  const n = sample.length;
+
+  //let matrix = Array.from({ length: rows }, () => new Array(cols).fill(0));
+
+  const matrixOne = Array.from({ length: n + 1 }, () =>
+    Array(numOfClasses + 1).fill(0)
+  );
+  const matrixTwo = Array.from({ length: n + 1 }, () =>
+    Array(numOfClasses + 1).fill(Number.MAX_VALUE)
+  );
+
+  for (let i = 1; i <= numOfClasses; i++) {
+    matrixOne[0][i] = 1;
+    matrixOne[1][i] = 1;
+    matrixTwo[0][i] = 0.0;
+
+    for (let j = 2; j <= n; j++) {
+      matrixTwo[j][i] = Number.MAX_VALUE;
+    }
+  }
+
+  for (let l = 2; l <= n; l++) {
+    let s1 = 0.0;
+    let s2 = 0.0;
+    let w = 0;
+    let v = 0.0;
+
+    for (let m = 1; m <= l; m++) {
+      const i3 = l - m + 1;
+
+      const val = sample[i3 - 1];
+
+      s2 += val * val;
+      s1 += val;
+      w++;
+
+      v = s2 - (s1 * s1) / w;
+      const i4 = i3 - 1;
+      if (i4 !== 0) {
+        for (let j = 2; j <= numOfClasses; j++) {
+          if (matrixTwo[l][j] >= v + matrixTwo[i4][j - 1]) {
+            matrixOne[l][j] = i4;
+            matrixTwo[l][j] = v + matrixTwo[i4][j - 1];
+          }
+        }
+      }
+    }
+    matrixOne[l][1] = 1;
+    matrixTwo[l][1] = v;
+  }
+
+  const breaks = Array(numOfClasses);
+  breaks[numOfClasses - 1] = sample[n - 1];
+
+  for (let j = numOfClasses, k = n; j >= 2; j--) {
+    const id = matrixOne[k][j] - 1;
+    breaks[j - 2] = sample[id];
+    k = matrixOne[k][j] = 1;
+  }
+
+  console.log('breaks', breaks);
+  return breaks;
+};
