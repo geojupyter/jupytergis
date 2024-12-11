0cd50c5ab32e3c308e2de2c50e4a488905e9cddf
Greg <gjmooney@asu.edu>
2024-12-11 11:16:13 +0100

    Remove unused import

--- packages/base/src/annotations/components/AnnotationFloater.tsx
+++ packages/base/src/annotations/components/AnnotationFloater.tsx
@@ -1,14 +1,5 @@
-import { IAnnotationModel } from '@jupytergis/schema';
-import { Dialog, showDialog } from '@jupyterlab/apputils';
-import { Button, deleteIcon } from '@jupyterlab/ui-components';
-import React, { useMemo, useState } from 'react';
-import { minimizeIcon } from '../../icons';
-import { Message } from './Message';
-import {
-  faPaperPlane,
-  faTrash,
-  faWindowMinimize
-} from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Annotation, { IAnnotationProps } from './Annotation';
 
@@ -21,8 +12,6 @@ const AnnotationFloater = ({
   model,
   open
 }: IAnnotationFloaterProps) => {
-  //   const { itemId, model } = props;
-
   const [isOpen, setIsOpen] = useState(open);
 
   // Function that either
