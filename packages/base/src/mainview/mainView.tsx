b83bfbcf7bddb5191e71231a92a9fe7bc82fdfeb
Greg <gjmooney@asu.edu>
2025-01-10 10:35:02 +0100

    Put stroke back

--- packages/base/src/mainview/mainView.tsx
+++ packages/base/src/mainview/mainView.tsx
@@ -839,7 +839,9 @@ export class MainView extends React.Component<IProps, IStates> {
       'stroke-color': '#3399CC',
       'stroke-width': 1.25,
       'circle-radius': 5,
-      'circle-fill-color': 'rgba(255,255,255,0.4)'
+      'circle-fill-color': 'rgba(255,255,255,0.4)',
+      'circle-stroke-width': 1.25,
+      'circle-stroke-color': '#3399CC'
     };
 
     const defaultRules: Rule = {
