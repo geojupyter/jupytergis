441db58404ebd4ac46a30772be2db43261894ba2
arjxn-py <arjxn.py@gmail.com>
2025-02-25 15:10:21 +0530

    configure some tests

---
+++ ui-tests/tests/lite.spec.ts
@@ -0,0 +1,95 @@
+import { expect, galata, test } from '@jupyterlab/galata';
+import path from 'path';
+
+test.use({ autoGoto: false });
+
+test.describe('UI Test', () => {
+  const fileList = ['test.jGIS', 'buildings.qgz', 'shapefile-test.jGIS'];
+
+  test.describe('File operations', () => {
+    let errors = 0;
+    test.beforeEach(async ({ page }) => {
+      const unrelatedErrors = [
+        // This error is related to plotly dependency, installed with qgis.
+        "@jupyter-widgets/base doesn't exist in shared scope default"
+      ];
+      page.setViewportSize({ width: 1920, height: 1080 });
+      page.on('console', message => {
+        if (message.type() === 'error') {
+          for (let pattern of unrelatedErrors) {
+            if (message.text().includes(pattern)) {
+              return;
+            }
+          }
+          console.log('CONSOLE ERROR', message);
+          errors += 1;
+        }
+      });
+    });
+
+    test.afterEach(async ({ page }) => {
+      errors = 0;
+    });
+
+    // for (const file of fileList) {
+    //   test(`Should be able to render ${file} without error`, async ({
+    //     page
+    //   }) => {
+    //     await page.goto();
+    //     const fullPath = `testDir/${file}`;
+    //     await page.notebook.openByPath(fullPath);
+    //     await page.notebook.activate(fullPath);
+    //     await page.locator('div.jGIS-Spinner').waitFor({ state: 'hidden' });
+    //     await page.waitForTimeout(1000);
+    //     if (await page.getByRole('button', { name: 'Ok' }).isVisible()) {
+    //       await page.getByRole('button', { name: 'Ok' }).click();
+    //     }
+    //     await page
+    //       .getByRole('tablist', { name: 'main sidebar' })
+    //       .getByRole('tab', { name: 'JupyterGIS Control Panel' })
+    //       .click();
+    //     await page
+    //       .getByRole('tablist', { name: 'alternate sidebar' })
+    //       .getByRole('tab', { name: 'JupyterGIS Control Panel' })
+    //       .click();
+    //     await page.waitForTimeout(1000);
+    //     expect(errors).toBe(0);
+
+    //     await expect(page.locator('.ol-unselectable').first()).toBeAttached();
+    //   });
+    // }
+
+    for (const file of fileList) {
+        test(`Should be able to render ${file} without error`, async ({
+          browser
+        }) => {
+          const context = await browser.newContext();
+          const page = await context.newPage();
+
+          await page.goto(`lab/index.html?path=${file}`, {
+            waitUntil: 'domcontentloaded'
+          });
+
+          await page.locator('div.jGIS-Spinner').waitFor({ state: 'hidden' });
+
+          if (await page.getByRole('button', { name: 'Ok' }).isVisible()) {
+            await page.getByRole('button', { name: 'Ok' }).click();
+          }
+
+          const main = await page.waitForSelector('.jp-MainAreaWidget', {
+            state: 'visible'
+          });
+
+          await page.waitForTimeout(10000);
+
+          expect(errors).toBe(0);
+          if (main) {
+            expect(await main.screenshot()).toMatchSnapshot({
+              name: `Render-${file}.png`,
+              maxDiffPixelRatio: 0.01
+            });
+          }
+        });
+    }
+  });
+});
