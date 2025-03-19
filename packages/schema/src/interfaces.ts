6e989b04e2a3e4738776a7cdad4f2525d23d4950
Arjun Verma <arjxn.py@gmail.com>
2025-03-19 12:17:56 +0530

    organise `schema/` (#542)
    * organise `schema/`
    
    * fix
    
    * not worth it
    
    * fix imports
    
    * fix python imports
    
    * typo
    
    * nahh
    
    * try fixing build
    
    * try fixing artifacts path
    
    * try fixing integration tests
    
    * Rebase
    
    * `layers/` & `sources/`
    
    * fix paths
    
    * fix path
    
    * fix python paths
    
    * `jgis/` -> `project/`
    
    * Update packages/schema/tsconfig.json
    
    Co-authored-by: Matt Fisher <3608264+mfisher87@users.noreply.github.com>
    
    * lint
    
    * fix python paths
    
    * integration test path
    
    ---------
    
    Co-authored-by: Matt Fisher <3608264+mfisher87@users.noreply.github.com>

--- packages/schema/src/interfaces.ts
+++ packages/schema/src/interfaces.ts
@@ -25,8 +25,8 @@ import {
   IJGISSource,
   IJGISSources,
   SourceType
-} from './_interface/jgis';
-import { IRasterSource } from './_interface/rastersource';
+} from './_interface/project/jgis';
+import { IRasterSource } from './_interface/project/sources/rastersource';
 export { IGeoJSONSource } from './_interface/geojsonsource';
 
 export type JgisCoordinates = { x: number; y: number };
