f044c1b7ccb76bd8cbf463b9636eb53af4b31f8a
Arjun Verma <arjunverma.oc@gmail.com>
2025-03-25 14:31:36 +0530

    Update packages/base/src/formbuilder/index.ts
    Co-authored-by: martinRenou <martin.renou@gmail.com>

--- packages/base/src/formbuilder/index.ts
+++ packages/base/src/formbuilder/index.ts
@@ -1,6 +1,4 @@
 export * from './objectform/baseform';
 export * from './objectform/source';
 export * from './objectform/layer';
-export * from './objectform/source';
-export * from './objectform/layer';
 export * from './creationform';
