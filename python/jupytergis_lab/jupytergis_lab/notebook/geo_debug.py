1840affbf61199b7c7bf6fe6dca184a7f54ca630
Matt Fisher <mfisher87@gmail.com>
2025-03-27 11:28:05 -0600

    Update TODO comments

--- python/jupytergis_lab/jupytergis_lab/notebook/geo_debug.py
+++ python/jupytergis_lab/jupytergis_lab/notebook/geo_debug.py
@@ -12,16 +12,13 @@ def geo_debug(geojson_path: str) -> None:
         "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
     )
 
-    # TODO: Support lots of file types
+    # TODO: Support lots of file types, and support Python objects like geodataframes.
     doc.add_geojson_layer(geojson_path)
 
     # TODO: Zoom to layer; is that feasible to do from Python? Currently not exposed in
     #       Python API.
 
     # TODO: Make map take up the whole sidebar space.
-    # TODO: Toolbar not visible.
-    #       /home/shared/jupytergis/packages/base/src/toolbar/widget.tsx
-    #
     # TODO: Activate left and right panel -- not sure how feasible yet from Python.
     #       Also, if using sidecar, right panel can't be displayed. Can we open a
     #       "native" JupyterLab pane instead of using sidecar?
