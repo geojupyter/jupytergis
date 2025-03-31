bffa2f9f1be45d7e5d2c541f23550c8679e564fd
Matt Fisher <mfisher87@gmail.com>
2025-03-31 16:05:54 -0600

    Restore nitpicky and fail on warning, update docs

--- docs/build.sh
+++ docs/build.sh
@@ -5,7 +5,9 @@ THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
 # Build can fail if certain artifacts exist here:
 rm -rf "${THIS_DIR}/_build"
 
-python -m sphinx --fail-on-warning --keep-going  --nitpicky --show-traceback \
+python -m sphinx \
+    --nitpicky --show-traceback \
+    --fail-on-warning --keep-going \
     --builder html --doctree-dir _build/doctrees --define language=en \
    . \
    ./_build/html
