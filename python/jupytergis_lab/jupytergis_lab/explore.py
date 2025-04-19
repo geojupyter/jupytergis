b649e4416e97ed2f11f4a6145eec4b05dda4cfb2
Matt Fisher <mfisher87@gmail.com>
2025-04-18 20:24:34 -0600

    Dynamically determine type of object passed to explore()
    Currently only support geodataframes and GeoJSON.

--- python/jupytergis_lab/jupytergis_lab/explore.py
+++ python/jupytergis_lab/jupytergis_lab/explore.py
@@ -1,24 +1,69 @@
 from pathlib import Path
+from typing import Any, Literal
+import re
 
 from jupytergis_lab import GISDocument
 
 
-def explore(geojson_path: str | Path) -> None:
+def explore(data: str | Path | Any) -> GISDocument:
     """Run a JupyterGIS data interaction interface alongside a Notebook.
 
-    :param geojson_path: Path to a GeoJSON file.
+    :param data: A GeoDataFrame or path to a GeoJSON file.
+
+    :raises FileNotFoundException: User passed a file that isn't present.
+    :raises NotImplementedError: User passed an input value that isn't supported yet.
+    :raises TypeError: User passed an object type that isn't supported.
+    :raises ValueError: User passed an object value that isn't supported.
     """
     doc = GISDocument()
 
-    # TODO: Basemap choices
+    # TODO: Basic basemap choices, e.g. add parameter `basemap: Literal["dark", "light"]`
     doc.add_raster_layer(
         "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
     )
 
-    # TODO: Support lots of file types, and support Python objects like geodataframes.
-    doc.add_geojson_layer(str(geojson_path))
+    # TODO: Extract to GISDocument class
+    _add_layer(doc, data)
 
-    # TODO: Zoom to layer; is that feasible to do from Python? Currently not exposed in
-    #       Python API.
+    # TODO: Zoom to layer. Currently not exposed in Python API.
 
     doc.sidecar(title="JupyterGIS explorer")
+
+    # TODO: should we return `doc`? It enables the exploration environment more usable,
+    # but by default, `explore(...)` would display a widget in the notebook _and_ open a
+    # sidecar for the same widget. The user would need to append a semicolon to disable
+    # that behavior. We can't disable that behavior from within this function to the
+    # best of my knowlwedge.
+
+
+def _add_layer(doc: GISDocument, data: str | Path | Any) -> None:
+    if isinstance(data, str):
+        if re.match(r"^(http|https)://", data) is not None:
+            raise NotImplementedError("URLs not yet supported.")
+        else:
+            data = Path(data)
+
+    if isinstance(data, Path):
+        if not data.exists():
+            raise FileNotFoundError(f"File not found: {data}")
+
+        ext = data.suffix.lower()
+
+        if ext in [".geojson", ".json"]:
+            doc.add_geojson_layer(path=data)
+            return
+        # TODO: elif ext in ['.tif', '.tiff']:
+        else:
+            raise ValueError(f"Unknown file type: {data}")
+
+    try:
+        from geopandas import GeoDataFrame
+
+        if isinstance(data, GeoDataFrame):
+            print(type(data.to_geo_dict()))
+            doc.add_geojson_layer(data=data.to_geo_dict())
+            return
+    except ImportError:
+        pass
+
+    raise TypeError(f"Unsupported input type: {type(data)}")
