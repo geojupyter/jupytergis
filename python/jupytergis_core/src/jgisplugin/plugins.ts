5b75d6855c0f4f148c9bfdea54faf6529e509bdc
arjxn-py <arjxn.py@gmail.com>
2025-04-29 19:43:50 +0530

    write schema version in document

--- python/jupytergis_core/src/jgisplugin/plugins.ts
+++ python/jupytergis_core/src/jgisplugin/plugins.ts
@@ -9,7 +9,8 @@ import {
   IJGISExternalCommandRegistryToken,
   IJupyterGISDocTracker,
   IJupyterGISWidget,
-  JupyterGISDoc
+  JupyterGISDoc,
+  SCHEMA_VERSION
 } from '@jupytergis/schema';
 import {
   JupyterFrontEnd,
@@ -155,7 +156,7 @@ const activate = (
         format: 'text',
         size: undefined,
         content:
-          '{\n\t"layers": {},\n\t"sources": {},\n\t"options": {"latitude": 0, "longitude": 0, "zoom": 0, "bearing": 0, "pitch": 0, "projection": "EPSG:3857"},\n\t"layerTree": [],\n\t"metadata": {}\n}'
+          `{\n\t"schemaVersion": "${SCHEMA_VERSION}",\n\t"layers": {},\n\t"sources": {},\n\t"options": {"latitude": 0, "longitude": 0, "zoom": 0, "bearing": 0, "pitch": 0, "projection": "EPSG:3857"},\n\t"layerTree": [],\n\t"metadata": {}\n}`
       });
 
       // Open the newly created file with the 'Editor'
