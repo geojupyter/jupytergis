cc5c66a872a82557babd205bd20f487272bbfeee
Florence Haudin <haudin.florence@gmail.com>
2025-05-19 12:49:59 +0200

    Add logics to use the source of the currently selected layer in the _handleDrawGeometryTypeChange method of the MainView class.

--- packages/schema/src/interfaces.ts
+++ packages/schema/src/interfaces.ts
@@ -237,6 +237,7 @@ export interface IJupyterGISModel extends DocumentRegistry.IModel {
   disposed: ISignal<any, void>;
   isDrawVectorLayerEnabled: boolean;
   updateIsDrawVectorLayerEnabled(): void;
+  selectedVectorLayerSourceId: string;
 }
 
 export interface IUserData {
--- packages/schema/src/model.ts
+++ packages/schema/src/model.ts
@@ -742,7 +742,6 @@ export class JupyterGISModel implements IJupyterGISModel {
 
   updateIsDrawVectorLayerEnabled() {
     this.drawVectorLayerChanged.emit(this.isDrawVectorLayerEnabled);
-    console.log('Signal emitted');
   }
 
   get geolocation(): JgisCoordinates {
@@ -801,6 +800,7 @@ export class JupyterGISModel implements IJupyterGISModel {
 
   public isDrawVectorLayerEnabled: boolean;
   public drawVectorLayerChanged = new Signal<this, boolean>(this);
+  public selectedVectorLayerSourceId = '';
 }
 
 export namespace JupyterGISModel {
