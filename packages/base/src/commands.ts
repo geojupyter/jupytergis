1a018b1597e10b68f69e9deb658e141a16cc0734
Florence Haudin <99649086+HaudinFlorence@users.noreply.github.com>
2025-05-20 11:26:48 +0200

    Update packages/base/src/commands.ts
    Co-authored-by: martinRenou <martin.renou@gmail.com>

--- packages/base/src/commands.ts
+++ packages/base/src/commands.ts
@@ -946,7 +946,7 @@ export function addCommands(
         return false;
       }
 
-      const canDrawVectorLayer = ['VectorLayer'].includes(selectedLayer.type);
+      const canDrawVectorLayer = selectedSource.type === 'GeoJSONSource' && selectedSource.data;
 
       if (
         tracker.currentWidget instanceof JupyterGISDocumentWidget &&
