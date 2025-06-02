7455aff1a138e84e9f937ccb0e13687313895d56
Greg <gregory.mooney@quantstack.net>
2025-06-02 12:30:07 +0200

    Move some stuff

--- packages/base/src/stacBrowser/components/StacPanelView.tsx
+++ packages/base/src/stacBrowser/components/StacPanelView.tsx
@@ -1,6 +1,11 @@
+import { IJGISLayer } from '@jupytergis/schema';
+import { UUID } from '@lumino/coreutils';
 import React, { useEffect, useState } from 'react';
 import { IStacViewProps } from '../StacBrowser';
+import { IStacQueryBody, IStacSearchResult } from '../types/types';
 import StacSections from './StacSection';
+
+const apiUrl = 'https://geodes-portal.cnes.fr/api/stac/search';
 
 const StacPanelView = ({
   datasets,
@@ -23,6 +28,110 @@ const StacPanelView = ({
 
   if (!model) {
     return <div>Loading model</div>;
+  }
+
+  const [isFirstRender, setIsFirstRender] = useState(false);
+
+  useEffect(() => {
+    // Geodes behavior => query on every selection
+    console.log('sc', selectedCollections);
+
+    console.log('sp', selectedPlatforms);
+
+    const selectedDatasets = Object.entries(datasets)
+      .filter(([key]) => selectedCollections.includes(key))
+      .flatMap(([_, values]) => values);
+
+    console.log('selectedDatasets', selectedDatasets);
+
+    // Build query
+    const fetchInEffect = async () => {
+      const body: IStacQueryBody = {
+        bbox: [-180, -65.76350697055292, 180, 65.76350697055292],
+        limit: 12,
+        page: 1,
+        query: {
+          dataset: {
+            in: selectedDatasets
+          },
+          end_datetime: {
+            gte: '2025-05-27T09:21:00.000Z'
+          },
+          latest: {
+            eq: true
+          },
+          // Only include platforms in query if there's a selection
+          ...(selectedPlatforms.length > 0 && {
+            platform: { in: selectedPlatforms }
+          })
+        },
+        sortBy: [
+          {
+            direction: 'desc',
+            field: 'start_datetime'
+          }
+        ]
+      };
+
+      console.log('body', body);
+
+      // TODO: Don't call this on render.
+      const result = await fetchWithProxy(body); // this result is ItemCollection
+      console.log('result', result);
+
+      // ! MAKEH DAH LAYAH
+      const layerId = UUID.uuid4();
+
+      const layerModel: IJGISLayer = {
+        type: 'StacLayer',
+        parameters: {
+          data: result
+        },
+        visible: true,
+        name: 'STAC Layer'
+      };
+
+      model.addLayer(layerId, layerModel);
+    };
+
+    // TODO: Do this better. Really don't use an effect
+    if (!isFirstRender) {
+      fetchInEffect();
+    }
+
+    if (isFirstRender) {
+      setIsFirstRender(false);
+    }
+  }, [selectedCollections, selectedPlatforms]);
+
+  async function fetchWithProxy(options: IStacQueryBody) {
+    // Needed for POST
+    const xsrfToken = document.cookie.match(/_xsrf=([^;]+)/)?.[1];
+
+    const proxyUrl = `/jupytergis_core/proxy?url=${encodeURIComponent(apiUrl)}`;
+
+    try {
+      const response = await fetch(proxyUrl, {
+        method: 'POST',
+        //@ts-expect-error Jupyter requires X-XSRFToken header
+        headers: {
+          'Content-Type': 'application/json',
+          'X-XSRFToken': xsrfToken,
+          credentials: 'include'
+        },
+        body: JSON.stringify(options)
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = (await response.json()) as IStacSearchResult;
+
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 
   return (
--- packages/base/src/stacBrowser/components/StacSection.tsx
+++ packages/base/src/stacBrowser/components/StacSection.tsx
@@ -1,12 +1,9 @@
-import { IDict, IJGISLayer, IJupyterGISModel } from '@jupytergis/schema';
-import { UUID } from '@lumino/coreutils';
-import React, { useEffect, useMemo, useState } from 'react';
+import { IDict, IJupyterGISModel } from '@jupytergis/schema';
+import React, { useMemo } from 'react';
 import {
   ToggleGroup,
   ToggleGroupItem
 } from '../../shared/components/ToggleGroup';
-import { datasets } from '../StacBrowser';
-import { IStacQueryBody, IStacSearchResult } from '../types/types';
 
 interface IStacCollectionsProps {
   header: string;
@@ -17,8 +14,6 @@ interface IStacCollectionsProps {
   model: IJupyterGISModel;
 }
 
-const apiUrl = 'https://geodes-portal.cnes.fr/api/stac/search';
-
 const StacSections = ({
   header,
   data,
@@ -28,109 +23,6 @@ const StacSections = ({
   model
 }: IStacCollectionsProps) => {
   // ! Starts here
-  const [isFirstRender, setIsFirstRender] = useState(false);
-
-  useEffect(() => {
-    // Geodes behavior => query on every selection
-    console.log('sc', selectedCollections);
-
-    console.log('sp', selectedPlatforms);
-
-    const selectedDatasets = Object.entries(datasets)
-      .filter(([key]) => selectedCollections.includes(key))
-      .flatMap(([_, values]) => values);
-
-    console.log('selectedDatasets', selectedDatasets);
-
-    // Build query
-    const fetchInEffect = async () => {
-      const body: IStacQueryBody = {
-        bbox: [-180, -65.76350697055292, 180, 65.76350697055292],
-        limit: 12,
-        page: 1,
-        query: {
-          dataset: {
-            in: selectedDatasets
-          },
-          end_datetime: {
-            gte: '2025-05-27T09:21:00.000Z'
-          },
-          latest: {
-            eq: true
-          },
-          // Only include platforms in query if there's a selection
-          ...(selectedPlatforms.length > 0 && {
-            platform: { in: selectedPlatforms }
-          })
-        },
-        sortBy: [
-          {
-            direction: 'desc',
-            field: 'start_datetime'
-          }
-        ]
-      };
-
-      console.log('body', body);
-
-      // TODO: Don't call this on render.
-      const result = await fetchWithProxy(body); // this result is ItemCollection
-      console.log('result', result);
-
-      // ! MAKEH DAH LAYAH
-      const layerId = UUID.uuid4();
-
-      const layerModel: IJGISLayer = {
-        type: 'StacLayer',
-        parameters: {
-          data: result
-        },
-        visible: true,
-        name: 'STAC Layer'
-      };
-
-      model.addLayer(layerId, layerModel);
-    };
-
-    // TODO: Do this better. Really don't use an effect
-    if (!isFirstRender) {
-      fetchInEffect();
-    }
-
-    if (isFirstRender) {
-      setIsFirstRender(false);
-    }
-  }, [selectedCollections, selectedPlatforms]);
-
-  async function fetchWithProxy(options: IStacQueryBody) {
-    // Needed for POST
-    const xsrfToken = document.cookie.match(/_xsrf=([^;]+)/)?.[1];
-
-    const proxyUrl = `/jupytergis_core/proxy?url=${encodeURIComponent(apiUrl)}`;
-
-    try {
-      const response = await fetch(proxyUrl, {
-        method: 'POST',
-        //@ts-expect-error Jupyter requires X-XSRFToken header
-        headers: {
-          'Content-Type': 'application/json',
-          'X-XSRFToken': xsrfToken,
-          credentials: 'include'
-        },
-        body: JSON.stringify(options)
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = (await response.json()) as IStacSearchResult;
-
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
 
   const items = useMemo(() => {
     if (header === 'Collection') {
