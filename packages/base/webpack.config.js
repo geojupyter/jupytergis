d032c14335d143e96cc6b99bb4602de2147dfc3c
Matt Fisher <mfisher87@gmail.com>
2025-06-03 12:47:15 -0600

    Support absolute paths with typescript-transform-paths and ts-patch
    Requires using `tspc` as compiler instead of `tsc`. Would be better to
    switch to the "use NX" approach of typescript-transform-paths, but I
    wasn't able to get that to work.
    
    Co-authored-by: martinRenou <martin.renou@gmail.com>

--- packages/base/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
-module.exports = {
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    plugins: [new TsconfigPathsPlugin({})]
-  }
-}
