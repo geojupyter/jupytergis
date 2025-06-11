3db205b0d9e0ddc4e087385172fd63bef88b1260
Greg <gregory.mooney@quantstack.net>
2025-06-11 16:35:45 +0200

    Add a STAC browser

---
+++ packages/base/src/stacBrowser/components/StacFilterSection.tsx
@@ -0,0 +1,87 @@
+import { IJupyterGISModel } from '@jupytergis/schema';
+import React, { useMemo } from 'react';
+import {
+  ToggleGroup,
+  ToggleGroupItem
+} from '../../shared/components/ToggleGroup';
+
+interface IStacCollectionsProps {
+  header: string;
+  data: Record<string, string[]>;
+  selectedCollections: string[];
+  selectedPlatforms: string[];
+  handleToggleGroupValueChange: (val: string[]) => void;
+  model: IJupyterGISModel;
+}
+
+const StacFilterSection = ({
+  header,
+  data,
+  selectedCollections,
+  selectedPlatforms,
+  handleToggleGroupValueChange,
+  model
+}: IStacCollectionsProps) => {
+  // ! Starts here
+
+  const items = useMemo(() => {
+    if (header === 'Collection') {
+      return Object.entries(data).map(([key, val]) => (
+        <ToggleGroupItem
+          key={key}
+          className="jgis-stac-browser-section-item"
+          value={key}
+        >
+          {key}
+        </ToggleGroupItem>
+      ));
+    }
+
+    if (header === 'Platform') {
+      return Object.entries(data)
+        .filter(([key]) => selectedCollections.includes(key))
+        .flatMap(([key, values]) =>
+          values.map(val => (
+            <ToggleGroupItem
+              key={`${key}-${val}`}
+              className="jgis-stac-browser-section-item"
+              value={val}
+            >
+              {val}
+            </ToggleGroupItem>
+          ))
+        );
+    }
+
+    return null;
+  }, [header, data, selectedCollections, selectedPlatforms]);
+
+  // Get the current selected values based on the header
+  const currentSelectedValues = useMemo(() => {
+    if (header === 'Collection') {
+      return selectedCollections;
+    }
+    if (header === 'Platform') {
+      return selectedPlatforms;
+    }
+    return [];
+  }, [header, selectedCollections, selectedPlatforms]);
+
+  return (
+    <div>
+      <span style={{ fontWeight: 'bold' }}>{header}</span>
+      <ToggleGroup
+        type="multiple"
+        variant={'outline'}
+        size={'sm'}
+        className="jgis-stac-browser-collection"
+        value={currentSelectedValues}
+        onValueChange={handleToggleGroupValueChange}
+      >
+        {items}
+      </ToggleGroup>
+    </div>
+  );
+};
+
+export default StacFilterSection;
---
+++ packages/base/src/stacBrowser/components/StacPanelFilters.tsx
@@ -0,0 +1,113 @@
+import { IJupyterGISModel } from '@jupytergis/schema';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import React from 'react';
+import { Button } from '../../shared/components/Button';
+import Calendar from '../../shared/components/Calendar';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '../../shared/components/Popover';
+import { IProductData } from '../types/types';
+import ProductSection from './ProductSection';
+import StacFilterSection from './StacFilterSection';
+
+interface IStacPanelFiltersProps {
+  datasets: Record<string, string[]>;
+  platforms: Record<string, string[]>;
+  products: Record<string, IProductData>;
+  selectedCollections: string[];
+  setSelectedCollections: (val: string[]) => void;
+  selectedPlatforms: string[];
+  setSelectedPlatforms: (val: string[]) => void;
+  selectedProducts: string[];
+  setSelectedProducts: (val: string[]) => void;
+  startTime: Date | undefined;
+  setStartTime: (date: Date | undefined) => void;
+  endTime: Date | undefined;
+  setEndTime: (date: Date | undefined) => void;
+  model: IJupyterGISModel;
+}
+
+const StacPanelFilters = ({
+  datasets,
+  platforms,
+  products,
+  selectedCollections,
+  setSelectedCollections,
+  selectedPlatforms,
+  setSelectedPlatforms,
+  selectedProducts,
+  setSelectedProducts,
+  startTime,
+  setStartTime,
+  endTime,
+  setEndTime,
+  model,
+}: IStacPanelFiltersProps) => {
+  return (
+    <div className="jgis-stac-browser-filters-panel">
+      <div className="jgis-stac-browser-date-picker">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant={'outline'}>
+              <CalendarIcon />
+              {startTime ? format(startTime, 'PPP') : <span>Start Date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <Calendar
+              mode="single"
+              selected={startTime}
+              onSelect={setStartTime}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant={'outline'}>
+              <CalendarIcon />
+              {endTime ? format(endTime, 'PPP') : <span>End Date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <Calendar
+              mode="single"
+              selected={endTime}
+              onSelect={setEndTime}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+      <StacFilterSection
+        header="Collection"
+        data={datasets}
+        selectedCollections={selectedCollections}
+        handleToggleGroupValueChange={setSelectedCollections}
+        selectedPlatforms={selectedPlatforms}
+        model={model}
+      />
+      <StacFilterSection
+        header="Platform"
+        data={platforms}
+        selectedCollections={selectedCollections}
+        handleToggleGroupValueChange={setSelectedPlatforms}
+        selectedPlatforms={selectedPlatforms}
+        model={model}
+      />
+      <ProductSection
+        header="Data / Product"
+        data={products}
+        selectedCollections={selectedCollections}
+        handleToggleGroupValueChange={setSelectedProducts}
+        selectedProducts={selectedProducts}
+        model={model}
+      />
+      {/* <div>cloud cover</div> */}
+    </div>
+  );
+};
+export default StacPanelFilters;
