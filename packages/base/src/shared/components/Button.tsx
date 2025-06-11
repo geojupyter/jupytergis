9ca82818daecd5b1db6815b7febf9b44c6df9eae
Greg Mooney <gregory.mooney@quantstack.net>
2025-06-11 14:13:51 +0200

    Create shared components (#749)
    * Create some shared components
    
    * Add tabs; Add CSS
    
    * Button CSS
    
    * Use newer button
    
    * Add components for date picker
    
    * Add pagination component
    
    * Iterate on paginator
    
    * Tiny tweaks
    
    * Use the correct package.json
    
    * lint
    
    * Woops
    
    * Use jupyter css vars
    
    * Add sr-only class
    
    * Lint
    
    * Clean up

---
+++ packages/base/src/shared/components/Button.tsx
@@ -0,0 +1,27 @@
+import { Slot } from '@radix-ui/react-slot';
+import * as React from 'react';
+
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+  variant?: 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link' | 'icon';
+  size?: 'sm' | 'lg' | 'icon';
+}
+
+const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
+  ({ variant, className, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        data-size={size}
+        data-variant={variant}
+        className={`Button ${className ? className : ''}`}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Button.displayName = 'Button';
+
+export { Button };
+export type { IButtonProps as ButtonProps };
