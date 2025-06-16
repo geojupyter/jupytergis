3dac426fd43040906579caf9f466d46cfc02b7f3
Greg <gregory.mooney@quantstack.net>
2025-06-16 11:22:58 +0200

    Update calendar and use UTC time

--- packages/base/src/shared/components/Calendar.tsx
+++ packages/base/src/shared/components/Calendar.tsx
@@ -1,88 +1,508 @@
+'use client';
+
+import { differenceInCalendarDays } from 'date-fns';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import * as React from 'react';
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
+import {
+  DayPicker,
+  labelNext,
+  labelPrevious,
+  useDayPicker,
+  type DayPickerProps,
+} from 'react-day-picker';
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+import { Button } from './Button';
+import { cn } from './utils';
 
-function Calendar({ showOutsideDays = true, ...props }: CalendarProps) {
+export type CalendarProps = DayPickerProps & {
+  /**
+   * In the year view, the number of years to display at once.
+   * @default 12
+   */
+  yearRange?: number;
+
+  /**
+   * Wether to show the year switcher in the caption.
+   * @default true
+   */
+  showYearSwitcher?: boolean;
+
+  monthsClassName?: string;
+  monthCaptionClassName?: string;
+  weekdaysClassName?: string;
+  weekdayClassName?: string;
+  monthClassName?: string;
+  captionClassName?: string;
+  captionLabelClassName?: string;
+  buttonNextClassName?: string;
+  buttonPreviousClassName?: string;
+  navClassName?: string;
+  monthGridClassName?: string;
+  weekClassName?: string;
+  dayClassName?: string;
+  dayButtonClassName?: string;
+  rangeStartClassName?: string;
+  rangeEndClassName?: string;
+  selectedClassName?: string;
+  todayClassName?: string;
+  outsideClassName?: string;
+  disabledClassName?: string;
+  rangeMiddleClassName?: string;
+  hiddenClassName?: string;
+};
+
+type NavView = 'days' | 'years';
+
+/**
+ * A custom calendar component built on top of react-day-picker.
+ * @param props The props for the calendar.
+ * @default yearRange 12
+ * @returns
+ */
+function Calendar({
+  className,
+  showOutsideDays = true,
+  showYearSwitcher = true,
+  yearRange = 12,
+  numberOfMonths,
+  components,
+  ...props
+}: CalendarProps) {
+  const [navView, setNavView] = React.useState<NavView>('days');
+  const [displayYears, setDisplayYears] = React.useState<{
+    from: number;
+    to: number;
+  }>(
+    React.useMemo(() => {
+      const currentYear = new Date().getFullYear();
+      return {
+        from: currentYear - Math.floor(yearRange / 2 - 1),
+        to: currentYear + Math.ceil(yearRange / 2),
+      };
+    }, [yearRange]),
+  );
+
+  const { onNextClick, onPrevClick, startMonth, endMonth } = props;
+
+  const columnsDisplayed = navView === 'years' ? 1 : numberOfMonths;
+
+  const _monthsClassName = cn('jgis-calendar-months', props.monthsClassName);
+  const _monthCaptionClassName = cn(
+    'jgis-calendar-month-caption',
+    props.monthCaptionClassName,
+  );
+  const _weekdaysClassName = cn(
+    'jgis-calendar-weekdays',
+    props.weekdaysClassName,
+  );
+  const _weekdayClassName = cn('jgis-calendar-weekday', props.weekdayClassName);
+  const _monthClassName = cn('jgis-calendar-month', props.monthClassName);
+  const _captionClassName = cn(
+    'jgis-calendar-month-caption',
+    props.captionClassName,
+  );
+  const _captionLabelClassName = cn(
+    'jgis-calendar-caption-label',
+    props.captionLabelClassName,
+  );
+  const _buttonNextClassName = cn(
+    'jgis-calendar-button-next',
+    props.buttonNextClassName,
+  );
+  const _buttonPreviousClassName = cn(
+    'jgis-calendar-button-previous',
+    props.buttonPreviousClassName,
+  );
+  const _navClassName = cn('jgis-calendar-nav', props.navClassName);
+  const _monthGridClassName = cn(
+    'jgis-calendar-month-grid',
+    props.monthGridClassName,
+  );
+  const _weekClassName = cn('jgis-calendar-week', props.weekClassName);
+  const _dayClassName = cn('jgis-calendar-day', props.dayClassName);
+  //   const _dayButtonClassName = cn(
+  //     'jgis-calendar-day-button',
+  //     props.dayButtonClassName,
+  //   );
+  const _rangeStartClassName = cn(
+    'jgis-calendar-day-button range-start',
+    props.rangeStartClassName,
+  );
+  const _rangeEndClassName = cn(
+    'jgis-calendar-day-button range-end',
+    props.rangeEndClassName,
+  );
+  const _rangeMiddleClassName = cn(
+    'jgis-calendar-range-middle',
+    props.rangeMiddleClassName,
+  );
+  const _selectedClassName = cn(
+    'jgis-calendar-day-selected',
+    props.selectedClassName,
+  );
+  const _todayClassName = cn('jgis-calendar-day-today', props.todayClassName);
+  const _outsideClassName = cn(
+    'jgis-calendar-day-outside',
+    props.outsideClassName,
+  );
+  const _disabledClassName = cn(
+    'jgis-calendar-day-disabled',
+    props.disabledClassName,
+  );
+  const _hiddenClassName = cn('jgis-calendar-hidden', props.hiddenClassName);
+
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      weekStartsOn={1}
+      className={cn('jgis-calendar-container', className)}
+      style={{
+        width: 248.8 * (columnsDisplayed ?? 1) + 'px',
+      }}
+      classNames={{
+        months: _monthsClassName,
+        month_caption: _monthCaptionClassName,
+        weekdays: _weekdaysClassName,
+        weekday: _weekdayClassName,
+        month: _monthClassName,
+        caption: _captionClassName,
+        caption_label: _captionLabelClassName,
+        button_next: _buttonNextClassName,
+        button_previous: _buttonPreviousClassName,
+        nav: _navClassName,
+        month_grid: _monthGridClassName,
+        week: _weekClassName,
+        day: _dayClassName,
+        // day_button: _dayButtonClassName,
+        range_start: _rangeStartClassName,
+        range_middle: _rangeMiddleClassName,
+        range_end: _rangeEndClassName,
+        selected: _selectedClassName,
+        today: _todayClassName,
+        outside: _outsideClassName,
+        disabled: _disabledClassName,
+        hidden: _hiddenClassName,
+      }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft color="currentColor" />,
-        IconRight: ({ ...props }) => <ChevronRight color="currentColor" />,
+        Chevron: ({ orientation }) => {
+          const Icon = orientation === 'left' ? ChevronLeft : ChevronRight;
+          return <Icon className="h-4 w-4" />;
+        },
+        Nav: ({ className }) => (
+          <Nav
+            className={className}
+            displayYears={displayYears}
+            navView={navView}
+            setDisplayYears={setDisplayYears}
+            startMonth={startMonth}
+            endMonth={endMonth}
+            onPrevClick={onPrevClick}
+            onNextClick={onNextClick}
+          />
+        ),
+        CaptionLabel: props => (
+          <CaptionLabel
+            showYearSwitcher={showYearSwitcher}
+            navView={navView}
+            setNavView={setNavView}
+            displayYears={displayYears}
+            {...props}
+          />
+        ),
+        MonthGrid: ({ className, children, ...props }) => (
+          <MonthGrid
+            children={children}
+            className={className}
+            displayYears={displayYears}
+            startMonth={startMonth}
+            endMonth={endMonth}
+            navView={navView}
+            setNavView={setNavView}
+            {...props}
+          />
+        ),
+        ...components,
       }}
-      modifiersStyles={{
-        selected: {
-          backgroundColor: 'var(--jp-layout-color2)',
-          color: 'var(--jp-ui-font-color0)',
-          borderRadius: '0.275rem',
-        },
-      }}
-      styles={{
-        root: {
-          width: 'max-content',
-          margin: 0,
-          color: 'var(--jp-ui-font-color0)',
-          background: 'var(--jp-layout-color0)',
-          border: '1px solid var(--jp-border-color0)',
-          borderRadius: 'var(--jp-border-radius)',
-          padding: '0.5rem',
-          position: 'relative',
-        },
-        table: {
-          paddingTop: '1rem',
-        },
-        head_cell: {
-          color: 'var(--jp-ui-font-color0)',
-          fontSize: '0.8rem',
-          fontWeight: 400,
-        },
-        day: {
-          backgroundColor: 'var(--jp-layout-color0)',
-          display: 'flex',
-          justifyContent: 'center',
-          border: 'none',
-          padding: '0.5rem',
-          margin: 'auto',
-          fontSize: '0.8rem',
-        },
-        caption: {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        nav_button_previous: {
-          color: 'var(--jp-ui-font-color0)',
-          background: 'transparent',
-          border: '0.5px solid var(--jp-border-color0)',
-          position: 'absolute',
-          top: '0.5rem',
-          left: '0.5rem',
-          borderRadius: 'var(--jp-border-radius)',
-          padding: '0.175rem',
-          width: 'max-content',
-          height: 'max-content',
-        },
-        nav_button_next: {
-          color: 'var(--jp-ui-font-color0)',
-          background: 'transparent',
-          border: '0.5px solid var(--jp-border-color0)',
-          position: 'absolute',
-          top: '0.5rem',
-          right: '0.5rem',
-          borderRadius: 'var(--jp-border-radius)',
-          padding: '0.175rem',
-          width: 'max-content',
-          height: 'max-content',
-        },
-      }}
+      numberOfMonths={columnsDisplayed}
+      timeZone="UTC"
       {...props}
     />
   );
 }
 Calendar.displayName = 'Calendar';
 
-export default Calendar;
+function Nav({
+  className,
+  navView,
+  startMonth,
+  endMonth,
+  displayYears,
+  setDisplayYears,
+  onPrevClick,
+  onNextClick,
+}: {
+  className?: string;
+  navView: NavView;
+  startMonth?: Date;
+  endMonth?: Date;
+  displayYears: { from: number; to: number };
+  setDisplayYears: React.Dispatch<
+    React.SetStateAction<{ from: number; to: number }>
+  >;
+  onPrevClick?: (date: Date) => void;
+  onNextClick?: (date: Date) => void;
+}) {
+  const { nextMonth, previousMonth, goToMonth } = useDayPicker();
+
+  const isPreviousDisabled = (() => {
+    if (navView === 'years') {
+      return (
+        (startMonth &&
+          differenceInCalendarDays(
+            new Date(displayYears.from - 1, 0, 1),
+            startMonth,
+          ) < 0) ||
+        (endMonth &&
+          differenceInCalendarDays(
+            new Date(displayYears.from - 1, 0, 1),
+            endMonth,
+          ) > 0)
+      );
+    }
+    return !previousMonth;
+  })();
+
+  const isNextDisabled = (() => {
+    if (navView === 'years') {
+      return (
+        (startMonth &&
+          differenceInCalendarDays(
+            new Date(displayYears.to + 1, 0, 1),
+            startMonth,
+          ) < 0) ||
+        (endMonth &&
+          differenceInCalendarDays(
+            new Date(displayYears.to + 1, 0, 1),
+            endMonth,
+          ) > 0)
+      );
+    }
+    return !nextMonth;
+  })();
+
+  const handlePreviousClick = React.useCallback(() => {
+    if (!previousMonth) {
+      return;
+    }
+    if (navView === 'years') {
+      setDisplayYears(prev => ({
+        from: prev.from - (prev.to - prev.from + 1),
+        to: prev.to - (prev.to - prev.from + 1),
+      }));
+      onPrevClick?.(
+        new Date(
+          displayYears.from - (displayYears.to - displayYears.from),
+          0,
+          1,
+        ),
+      );
+      return;
+    }
+    goToMonth(previousMonth);
+    onPrevClick?.(previousMonth);
+  }, [previousMonth, goToMonth]);
+
+  const handleNextClick = React.useCallback(() => {
+    if (!nextMonth) {
+      return;
+    }
+    if (navView === 'years') {
+      setDisplayYears(prev => ({
+        from: prev.from + (prev.to - prev.from + 1),
+        to: prev.to + (prev.to - prev.from + 1),
+      }));
+      onNextClick?.(
+        new Date(
+          displayYears.from + (displayYears.to - displayYears.from),
+          0,
+          1,
+        ),
+      );
+      return;
+    }
+    goToMonth(nextMonth);
+    onNextClick?.(nextMonth);
+  }, [goToMonth, nextMonth]);
+  return (
+    <nav className={cn('jgis-calendar-nav', className)}>
+      <Button
+        variant="ghost"
+        className="jgis-calendar-button-previous"
+        type="button"
+        tabIndex={isPreviousDisabled ? undefined : -1}
+        disabled={isPreviousDisabled}
+        aria-label={
+          navView === 'years'
+            ? `Go to the previous ${
+                displayYears.to - displayYears.from + 1
+              } years`
+            : labelPrevious(previousMonth)
+        }
+        onClick={handlePreviousClick}
+      >
+        <ChevronLeft className="h-4 w-4" />
+      </Button>
+
+      <Button
+        variant="ghost"
+        className="jgis-calendar-button-next"
+        type="button"
+        tabIndex={isNextDisabled ? undefined : -1}
+        disabled={isNextDisabled}
+        aria-label={
+          navView === 'years'
+            ? `Go to the next ${displayYears.to - displayYears.from + 1} years`
+            : labelNext(nextMonth)
+        }
+        onClick={handleNextClick}
+      >
+        <ChevronRight className="h-4 w-4" />
+      </Button>
+    </nav>
+  );
+}
+
+function CaptionLabel({
+  children,
+  showYearSwitcher,
+  navView,
+  setNavView,
+  displayYears,
+  ...props
+}: {
+  showYearSwitcher?: boolean;
+  navView: NavView;
+  setNavView: React.Dispatch<React.SetStateAction<NavView>>;
+  displayYears: { from: number; to: number };
+} & React.HTMLAttributes<HTMLSpanElement>) {
+  if (!showYearSwitcher) {
+    return <span {...props}>{children}</span>;
+  }
+  return (
+    <Button
+      className="jgis-calendar-caption-button"
+      variant="ghost"
+      size="sm"
+      onClick={() => setNavView(prev => (prev === 'days' ? 'years' : 'days'))}
+    >
+      {navView === 'days'
+        ? children
+        : displayYears.from + ' - ' + displayYears.to}
+    </Button>
+  );
+}
+
+function MonthGrid({
+  className,
+  children,
+  displayYears,
+  startMonth,
+  endMonth,
+  navView,
+  setNavView,
+  ...props
+}: {
+  className?: string;
+  children: React.ReactNode;
+  displayYears: { from: number; to: number };
+  startMonth?: Date;
+  endMonth?: Date;
+  navView: NavView;
+  setNavView: React.Dispatch<React.SetStateAction<NavView>>;
+} & React.TableHTMLAttributes<HTMLTableElement>) {
+  if (navView === 'years') {
+    return (
+      <YearGrid
+        displayYears={displayYears}
+        startMonth={startMonth}
+        endMonth={endMonth}
+        setNavView={setNavView}
+        navView={navView}
+        className={className}
+        {...props}
+      />
+    );
+  }
+  return (
+    <table className={className} {...props}>
+      {children}
+    </table>
+  );
+}
+
+function YearGrid({
+  className,
+  displayYears,
+  startMonth,
+  endMonth,
+  setNavView,
+  navView,
+  ...props
+}: {
+  className?: string;
+  displayYears: { from: number; to: number };
+  startMonth?: Date;
+  endMonth?: Date;
+  setNavView: React.Dispatch<React.SetStateAction<NavView>>;
+  navView: NavView;
+} & React.HTMLAttributes<HTMLDivElement>) {
+  const { goToMonth, selected } = useDayPicker();
+
+  return (
+    <div className={cn('jgis-calendar-year-grid', className)} {...props}>
+      {Array.from(
+        { length: displayYears.to - displayYears.from + 1 },
+        (_, i) => {
+          const isBefore =
+            differenceInCalendarDays(
+              new Date(displayYears.from + i, 11, 31),
+              startMonth!,
+            ) < 0;
+
+          const isAfter =
+            differenceInCalendarDays(
+              new Date(displayYears.from + i, 0, 0),
+              endMonth!,
+            ) > 0;
+
+          const isDisabled = isBefore || isAfter;
+          return (
+            <Button
+              key={i}
+              className={cn(
+                'jgis-calendar-year-button',
+                displayYears.from + i === new Date().getFullYear() &&
+                  'jgis-calendar-year-button-current',
+                isDisabled && 'jgis-calendar-year-button-disabled',
+              )}
+              variant="ghost"
+              onClick={() => {
+                setNavView('days');
+                goToMonth(
+                  new Date(
+                    displayYears.from + i,
+                    (selected as Date | undefined)?.getMonth() ?? 0,
+                  ),
+                );
+              }}
+              disabled={navView === 'years' ? isDisabled : undefined}
+            >
+              {displayYears.from + i}
+            </Button>
+          );
+        },
+      )}
+    </div>
+  );
+}
+
+export { Calendar };
