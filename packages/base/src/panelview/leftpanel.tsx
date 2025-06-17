4bb59b25fbe8ba4824ef565d1b566ca0050d5491
Greg <gregory.mooney@quantstack.net>
2025-06-17 09:42:21 +0200

    Move STAC browser to existing panel

--- packages/base/src/panelview/leftpanel.tsx
+++ packages/base/src/panelview/leftpanel.tsx
@@ -16,6 +16,7 @@ import { IControlPanelModel } from '@/src/types';
 import { FilterPanel } from './components/filter-panel/Filter';
 import { LayersPanel } from './components/layers';
 import { ControlPanelHeader } from './header';
+import StacPanel from '../stacBrowser/StacPanel';
 
 /**
  * Options of the left panel widget.
@@ -55,9 +56,19 @@ export class LeftPanelWidget extends SidePanel {
       state: this._state,
       onSelect: this._onSelect,
     });
+
     layerTree.title.caption = 'Layer tree';
     layerTree.title.label = 'Layers';
     this.addWidget(layerTree);
+
+    const stacPanel = new StacPanel({
+      model: this._model,
+      tracker: options.tracker,
+    });
+
+    stacPanel.title.caption = 'STAC';
+    stacPanel.title.label = 'STAC';
+    this.addWidget(stacPanel);
 
     const filterPanel = new FilterPanel({
       model: this._model,
