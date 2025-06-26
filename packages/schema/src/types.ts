3d1adfa73c150aa393917671550395370ee548c1
Matt Fisher <3608264+mfisher87@users.noreply.github.com>
2025-06-26 00:53:44 -0600

    Improve file naming consistency of schemas (#786)
    * Improve naming consistency of layer schemas
    
    * Rename source schemas
    
    * Rename export schemas
    
    * Update imports I missed

--- packages/schema/src/types.ts
+++ packages/schema/src/types.ts
@@ -1,19 +1,19 @@
 export * from './_interface/project/jgis';
 
 // Sources
+export * from './_interface/geoJsonSource';
 export * from './_interface/project/sources/geoTiffSource';
-export * from './_interface/geojsonsource';
 export * from './_interface/project/sources/imageSource';
 export * from './_interface/project/sources/rasterDemSource';
-export * from './_interface/project/sources/rastersource';
+export * from './_interface/project/sources/rasterSource';
 export * from './_interface/project/sources/shapefileSource';
-export * from './_interface/project/sources/vectortilesource';
+export * from './_interface/project/sources/vectorTileSource';
 export * from './_interface/project/sources/videoSource';
 
 // Layers
 export * from './_interface/project/layers/hillshadeLayer';
-export * from './_interface/project/layers/rasterlayer';
-export * from './_interface/project/layers/vectorlayer';
+export * from './_interface/project/layers/rasterLayer';
+export * from './_interface/project/layers/vectorLayer';
 export * from './_interface/project/layers/vectorTileLayer';
 export * from './_interface/project/layers/webGlLayer';
 export * from './_interface/project/layers/imageLayer';
@@ -23,8 +23,8 @@ export * from './_interface/project/layers/heatmapLayer';
 export * from './processing/_generated/exportProcessingSchema';
 
 // exportLayer
-export * from './_interface/export/exportGeojson';
-export * from './_interface/export/exportGeotiff';
+export * from './_interface/export/exportGeoJson';
+export * from './_interface/export/exportGeoTiff';
 
 // Other
 export * from './doc';
