33292e249ea01b8d09c2601495a90499467f2c73
Greg <gregory.mooney@quantstack.net>
2025-07-09 12:14:22 +0200

    Add cn to components

--- packages/base/src/shared/components/Badge.tsx
+++ packages/base/src/shared/components/Badge.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 
+import { cn } from './utils';
+
 interface IBadgeProps extends React.HTMLAttributes<HTMLButtonElement> {
   variant?: 'destructive' | 'outline' | 'secondary';
   size?: 'sm' | 'lg' | 'icon';
 }
 
-function Badge({ variant, ...props }: IBadgeProps) {
+function Badge({ variant, className, ...props }: IBadgeProps) {
   return (
     // @ts-expect-error lol
-    <div data-variant={variant} className={'jgis-badge'} {...props} />
+    <div
+      data-variant={variant}
+      className={cn('jgis-badge', className)}
+      {...props}
+    />
   );
 }
 
