f885ec8afa95bd2e8d185d33e2d91e15b54e23fa
Gauss_Taylor <gausstayloreuler@gmail.com>
2025-07-22 14:17:02 +0200

    Lint

--- packages/base/src/panelview/rightpanel.tsx
+++ packages/base/src/panelview/rightpanel.tsx
@@ -1,7 +1,7 @@
 import {
-	IAnnotationModel,
-	IJGISFormSchemaRegistry,
-	IJupyterGISModel,
+  IAnnotationModel,
+  IJGISFormSchemaRegistry,
+  IJupyterGISModel,
 } from '@jupytergis/schema';
 import * as React from 'react';
 
@@ -9,95 +9,77 @@ import { AnnotationsPanel } from './annotationPanel';
 import { IdentifyPanelComponent } from './components/identify-panel/IdentifyPanel';
 import { ObjectPropertiesReact } from './objectproperties';
 import {
-	PanelTabs,
-	TabsContent,
-	TabsList,
-	TabsTrigger,
+  PanelTabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
 } from '../shared/components/Tabs';
 
 interface IRightComponentProps {
-	formSchemaRegistry: IJGISFormSchemaRegistry;
-	annotationModel: IAnnotationModel;
-	model: IJupyterGISModel;
+  formSchemaRegistry: IJGISFormSchemaRegistry;
+  annotationModel: IAnnotationModel;
+  model: IJupyterGISModel;
 }
 
 export const RightPanelComponent = (options: IRightComponentProps) => {
-	const [selectedObjectProperties, setSelectedObjectProperties] =
-		React.useState(undefined);
+  const [selectedObjectProperties, setSelectedObjectProperties] =
+    React.useState(undefined);
 
-	const tabInfo = [
-		{ name: 'objectProperties', title: 'Object Properties' },
-		{ name: 'annotations', title: 'Annotations' },
-		{ name: 'identifyPanel', title: 'Identify Panels' },
-	];
+  const tabInfo = [
+    { name: 'objectProperties', title: 'Object Properties' },
+    { name: 'annotations', title: 'Annotations' },
+    { name: 'identifyPanel', title: 'Identify Panels' },
+  ];
 
-	const [curTab, setCurTab] = React.useState<string | undefined>(
-		tabInfo[0].name,
-	);
+  const [curTab, setCurTab] = React.useState<string | undefined>(
+    tabInfo[0].name,
+  );
 
-	return (
-		<div className="jgis-right-panel-container">
-			<PanelTabs className="jgis-panel-tabs" curTab={curTab}>
-				<TabsList>
-					{tabInfo.map(e => {
-						return (
-							<TabsTrigger
-								className="jGIS-layer-browser-category"
-								value={e.name}
-								onClick={() => {
-									if (
-										curTab !==
-										e.name
-									) {
-										setCurTab(
-											e.name,
-										);
-									} else {
-										setCurTab(
-											'',
-										);
-									}
-								}}
-							>
-								{e.title}
-							</TabsTrigger>
-						);
-					})}
-				</TabsList>
-				<TabsContent
-					value="objectProperties"
-					className="jgis-panel-tab-content"
-				>
-					<ObjectPropertiesReact
-						setSelectedObject={
-							setSelectedObjectProperties
-						}
-						selectedObject={
-							selectedObjectProperties
-						}
-						formSchemaRegistry={
-							options.formSchemaRegistry
-						}
-						model={options.model}
-					/>
-				</TabsContent>
-				<TabsContent value="annotations">
-					<AnnotationsPanel
-						annotationModel={
-							options.annotationModel
-						}
-						rightPanelModel={options.model}
-					></AnnotationsPanel>
-				</TabsContent>
-				<TabsContent
-					value="identifyPanel"
-					className="jgis-panel-tab-content"
-				>
-					<IdentifyPanelComponent
-						model={options.model}
-					></IdentifyPanelComponent>
-				</TabsContent>
-			</PanelTabs>
-		</div>
-	);
+  return (
+    <div className="jgis-right-panel-container">
+      <PanelTabs className="jgis-panel-tabs" curTab={curTab}>
+        <TabsList>
+          {tabInfo.map(e => {
+            return (
+              <TabsTrigger
+                className="jGIS-layer-browser-category"
+                value={e.name}
+                onClick={() => {
+                  if (curTab !== e.name) {
+                    setCurTab(e.name);
+                  } else {
+                    setCurTab('');
+                  }
+                }}
+              >
+                {e.title}
+              </TabsTrigger>
+            );
+          })}
+        </TabsList>
+        <TabsContent
+          value="objectProperties"
+          className="jgis-panel-tab-content"
+        >
+          <ObjectPropertiesReact
+            setSelectedObject={setSelectedObjectProperties}
+            selectedObject={selectedObjectProperties}
+            formSchemaRegistry={options.formSchemaRegistry}
+            model={options.model}
+          />
+        </TabsContent>
+        <TabsContent value="annotations">
+          <AnnotationsPanel
+            annotationModel={options.annotationModel}
+            rightPanelModel={options.model}
+          ></AnnotationsPanel>
+        </TabsContent>
+        <TabsContent value="identifyPanel" className="jgis-panel-tab-content">
+          <IdentifyPanelComponent
+            model={options.model}
+          ></IdentifyPanelComponent>
+        </TabsContent>
+      </PanelTabs>
+    </div>
+  );
 };
--- packages/base/src/stacBrowser/components/StacPanelView.tsx
+++ packages/base/src/stacBrowser/components/StacPanelView.tsx
@@ -2,83 +2,78 @@ import { IJupyterGISModel } from '@jupytergis/schema';
 import React from 'react';
 
 import {
-	Tabs,
-	TabsContent,
-	TabsList,
-	TabsTrigger,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
 } from '@/src/shared/components/Tabs';
 import useStacSearch from '@/src/stacBrowser/hooks/useStacSearch';
 import StacPanelFilters from './StacPanelFilters';
 import StacPanelResults from './StacPanelResults';
 
 interface IStacViewProps {
-	model?: IJupyterGISModel;
+  model?: IJupyterGISModel;
 }
 const StacPanelView = ({ model }: IStacViewProps) => {
-	const {
-		filterState,
-		filterSetters,
-		results,
-		startTime,
-		setStartTime,
-		endTime,
-		setEndTime,
-		totalPages,
-		currentPage,
-		totalResults,
-		handlePaginationClick,
-		handleResultClick,
-		formatResult,
-		isLoading,
-		useWorldBBox,
-		setUseWorldBBox,
-	} = useStacSearch({ model });
+  const {
+    filterState,
+    filterSetters,
+    results,
+    startTime,
+    setStartTime,
+    endTime,
+    setEndTime,
+    totalPages,
+    currentPage,
+    totalResults,
+    handlePaginationClick,
+    handleResultClick,
+    formatResult,
+    isLoading,
+    useWorldBBox,
+    setUseWorldBBox,
+  } = useStacSearch({ model });
 
-	if (!model) {
-		return null;
-	}
+  if (!model) {
+    return null;
+  }
 
-	return (
-		<Tabs defaultValue="filters" className="jgis-panel-tabs">
-			<TabsList style={{ borderRadius: 0 }}>
-				<TabsTrigger
-					className="jGIS-layer-browser-category"
-					value="filters"
-				>
-					Filters
-				</TabsTrigger>
-				<TabsTrigger
-					className="jGIS-layer-browser-category"
-					value="results"
-				>{`Results (${totalResults})`}</TabsTrigger>
-			</TabsList>
-			<TabsContent value="filters">
-				<StacPanelFilters
-					filterState={filterState}
-					filterSetters={filterSetters}
-					startTime={startTime}
-					setStartTime={setStartTime}
-					endTime={endTime}
-					setEndTime={setEndTime}
-					useWorldBBox={useWorldBBox}
-					setUseWorldBBox={setUseWorldBBox}
-				/>
-			</TabsContent>
-			<TabsContent value="results">
-				<StacPanelResults
-					results={results}
-					currentPage={currentPage}
-					totalPages={totalPages}
-					handlePaginationClick={
-						handlePaginationClick
-					}
-					handleResultClick={handleResultClick}
-					formatResult={formatResult}
-					isLoading={isLoading}
-				/>
-			</TabsContent>
-		</Tabs>
-	);
+  return (
+    <Tabs defaultValue="filters" className="jgis-panel-tabs">
+      <TabsList style={{ borderRadius: 0 }}>
+        <TabsTrigger className="jGIS-layer-browser-category" value="filters">
+          Filters
+        </TabsTrigger>
+        <TabsTrigger
+          className="jGIS-layer-browser-category"
+          value="results"
+        >{`Results (${totalResults})`}</TabsTrigger>
+      </TabsList>
+      <TabsContent value="filters">
+        <StacPanelFilters
+          filterState={filterState}
+          filterSetters={filterSetters}
+          startTime={startTime}
+          setStartTime={setStartTime}
+          endTime={endTime}
+          setEndTime={setEndTime}
+          useWorldBBox={useWorldBBox}
+          setUseWorldBBox={setUseWorldBBox}
+        />
+      </TabsContent>
+      <TabsContent value="results">
+        <StacPanelResults
+          results={results}
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePaginationClick={handlePaginationClick}
+          handleResultClick={handleResultClick}
+          formatResult={formatResult}
+          isLoading={isLoading}
+        />
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default StacPanelView;
