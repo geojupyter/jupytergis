720c7565880ff478973ff803cf60edcc0cb867c9
Gauss_Taylor <gausstayloreuler@gmail.com>
2025-07-22 14:16:10 +0200

    Made suggested changes

--- packages/base/src/panelview/leftpanel.tsx
+++ packages/base/src/panelview/leftpanel.tsx
@@ -6,10 +6,10 @@ import * as React from 'react';
 
 import { LayersBodyComponent } from './components/layers';
 import {
-  PanelTabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
+	PanelTabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
 } from '../shared/components/Tabs';
 import StacBrowser from '../stacBrowser/StacBrowser';
 import FilterComponent from './components/filter-panel/Filter';
@@ -18,73 +18,97 @@ import FilterComponent from './components/filter-panel/Filter';
  * Options of the left panel widget.
  */
 export interface ILeftPanelOptions {
-  model: IJupyterGISModel;
-  onSelect: ({ type, item, nodeId }: ILeftPanelClickHandlerParams) => void;
+	model: IJupyterGISModel;
+	onSelect: ({
+		type,
+		item,
+		nodeId,
+	}: ILeftPanelClickHandlerParams) => void;
 }
 
 export interface ILayerPanelOptions extends ILeftPanelOptions {
-  state: IStateDB;
+	state: IStateDB;
 }
 
 export interface ILeftPanelClickHandlerParams {
-  type: SelectionType;
-  item: string;
-  nodeId?: string;
-  event: ReactMouseEvent;
+	type: SelectionType;
+	item: string;
+	nodeId?: string;
+	event: ReactMouseEvent;
 }
 
 interface ILeftComponentProps {
-  model: IJupyterGISModel;
-  state: IStateDB;
-  commands: CommandRegistry;
+	model: IJupyterGISModel;
+	state: IStateDB;
+	commands: CommandRegistry;
 }
 
 export const LeftPanelComponent = (options: ILeftComponentProps) => {
-  const tabInfo = [
-    { name: 'layers', title: 'Layers' },
-    { name: 'stac', title: 'Stac Browser' },
-    { name: 'filters', title: 'Filters' },
-  ];
-  const [curTab, setCurTab] = React.useState<string | undefined>(
-    tabInfo[0].name,
-  );
+	const tabInfo = [
+		{ name: 'layers', title: 'Layers' },
+		{ name: 'stac', title: 'Stac Browser' },
+		{ name: 'filters', title: 'Filters' },
+	];
+	const [curTab, setCurTab] = React.useState<string | undefined>(
+		tabInfo[0].name,
+	);
 
-  return (
-    <div className="jgis-left-panel-container">
-      <PanelTabs curTab={curTab} className="jgis-stac-panel-main">
-        <TabsList>
-          {tabInfo.map(e => {
-            return (
-              <TabsTrigger
-                className="jGIS-layer-browser-category"
-                value={e.name}
-                onClick={() => {
-                  if (curTab !== e.name) {
-                    setCurTab(e.name);
-                  } else {
-                    setCurTab('');
-                  }
-                }}
-              >
-                {e.title}
-              </TabsTrigger>
-            );
-          })}
-        </TabsList>
-        <TabsContent value="layers" className="jgis-panel-tab-content">
-          <LayersBodyComponent
-            model={options.model}
-            commands={options.commands}
-            state={options.state}
-          ></LayersBodyComponent>
-        </TabsContent>
-        <TabsContent value="stac">
-          <StacBrowser controlPanelModel={options.model}></StacBrowser>
-        </TabsContent>
-        <TabsContent value="filters" className="jgis-panel-tab-content">
-          <FilterComponent model={options.model}></FilterComponent>,
-        </TabsContent>
-      </PanelTabs>
-    </div>
-  );
+	return (
+		<div className="jgis-left-panel-container">
+			<PanelTabs curTab={curTab} className="jgis-panel-tabs">
+				<TabsList>
+					{tabInfo.map(e => {
+						return (
+							<TabsTrigger
+								className="jGIS-layer-browser-category"
+								value={e.name}
+								onClick={() => {
+									if (
+										curTab !==
+										e.name
+									) {
+										setCurTab(
+											e.name,
+										);
+									} else {
+										setCurTab(
+											'',
+										);
+									}
+								}}
+							>
+								{e.title}
+							</TabsTrigger>
+						);
+					})}
+				</TabsList>
+				<TabsContent
+					value="layers"
+					className="jgis-panel-tab-content"
+				>
+					<LayersBodyComponent
+						model={options.model}
+						commands={options.commands}
+						state={options.state}
+					></LayersBodyComponent>
+				</TabsContent>
+				<TabsContent value="stac">
+					<StacBrowser
+						controlPanelModel={
+							options.model
+						}
+					></StacBrowser>
+				</TabsContent>
+				<TabsContent
+					value="filters"
+					className="jgis-panel-tab-content"
+				>
+					<FilterComponent
+						model={options.model}
+					></FilterComponent>
+					,
+				</TabsContent>
+			</PanelTabs>
+		</div>
+	);
 };
