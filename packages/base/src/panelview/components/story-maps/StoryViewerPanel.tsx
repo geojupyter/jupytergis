f76a0037d87ba3c7178e7d60744a2367a98baea7
Greg <gregory.mooney@quantstack.net>
2025-10-29 13:05:08 +0100

    Add temp viewer panel

--- packages/base/src/panelview/components/story-maps/StoryViewerPanel.tsx
+++ packages/base/src/panelview/components/story-maps/StoryViewerPanel.tsx
@@ -1,4 +1,8 @@
-import { IDict, IJupyterGISModel } from '@jupytergis/schema';
+import {
+  IJGISStoryMap,
+  IJupyterGISModel,
+  ILandmarkLayer,
+} from '@jupytergis/schema';
 import React, { useEffect, useState } from 'react';
 
 interface IStoryViewerPanelProps {
@@ -6,24 +10,55 @@ interface IStoryViewerPanelProps {
 }
 
 function StoryViewerPanel({ model }: IStoryViewerPanelProps) {
-  const [storyData, setStoryData] = useState<IDict>({});
-  console.log('storyData', storyData);
+  const [storyData, setStoryData] = useState<IJGISStoryMap>({});
+  const [activeSlide, setActiveSlide] = useState<ILandmarkLayer | undefined>(
+    undefined,
+  );
+
   //TODO this is copied from the editor panel, do better
   useEffect(() => {
     // Set initial data (you may need to get this from the model)
-    const firstStory = Array.from(model.storiesMap.values())[0];
-    const initialData: IDict = firstStory ?? {
-      title: '',
-      storyType: 'guided',
-      landmarks: [] as string[],
-    };
+    // Set initial data (you may need to get this from the model)
+    const entries = Object.entries(model.sharedModel.storiesMap);
+    const [firstKey, firstStory] = entries[0] ?? [undefined, undefined];
+    console.log('firstKey', firstKey);
 
-    setStoryData(initialData);
-  }, [model]);
+    // need to build story
+    // step 1: layerIds -> layers
+    const layers = firstStory.landmarks?.map(landmarkId =>
+      model.getLayer(landmarkId),
+    );
+
+    console.log('layers', layers);
+
+    // sort the landmarks for a guided tour
+    firstStory.storyType === 'guided' &&
+      layers?.sort((a, b) => {
+        if (!a || !b) {
+          return 0;
+        }
+        const aParams = a.parameters as ILandmarkLayer;
+        const bParams = b.parameters as ILandmarkLayer;
+        const aRank = aParams?.rank ?? Number.MAX_SAFE_INTEGER;
+        const bRank = bParams?.rank ?? Number.MAX_SAFE_INTEGER;
+        return aRank - bRank;
+      });
+
+    console.log('sorted layers', layers);
+    if (layers?.[0]) {
+      setActiveSlide(layers[0].parameters as ILandmarkLayer);
+    }
+
+    setStoryData(firstStory);
+  }, []);
   return (
     <div>
       {/* title */}
+      <span>{storyData.title}</span>
       {/* content */}
+      <div>{activeSlide?.content?.imgSrc}</div>
+      <div>{activeSlide?.content?.title}</div>
+      <div>{activeSlide?.content?.markdown}</div>
       {/* if guided -> nav buttons */}
     </div>
   );
