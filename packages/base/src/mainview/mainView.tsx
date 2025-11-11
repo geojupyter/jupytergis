6bebfeef4672f694ba1480475fb54892b640472b
Greg <gregory.mooney@quantstack.net>
2025-11-11 14:27:20 +0100

    Use icon for marker

--- packages/base/src/mainview/mainView.tsx
+++ packages/base/src/mainview/mainView.tsx
@@ -84,7 +84,7 @@ import {
 } from 'ol/source';
 import Static from 'ol/source/ImageStatic';
 import { TileSourceEvent } from 'ol/source/Tile';
-import { Circle, Fill, Stroke, Style } from 'ol/style';
+import { Circle, Fill, Icon, Stroke, Style } from 'ol/style';
 import { Rule } from 'ol/style/flat';
 //@ts-expect-error no types for ol-pmtiles
 import { PMTilesRasterSource, PMTilesVectorSource } from 'ol-pmtiles';
@@ -102,6 +102,7 @@ import CollaboratorPointers, { ClientPointer } from './CollaboratorPointers';
 import { FollowIndicator } from './FollowIndicator';
 import TemporalSlider from './TemporalSlider';
 import { MainViewModel } from './mainviewmodel';
+import { markerIcon } from '../icons';
 import { LeftPanel, RightPanel } from '../panelview';
 
 type OlLayerTypes =
@@ -836,6 +837,19 @@ export class MainView extends React.Component<IProps, IStates> {
           type: 'icon',
           geometry: point,
         });
+
+        // Replace color placeholder in SVG with the parameter color
+        const markerColor = parameters.color || '#3463a0';
+        const svgString = markerIcon.svgstr.replace('{{COLOR}}', markerColor);
+
+        const iconStyle = new Style({
+          image: new Icon({
+            src: `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svgString)}`,
+            scale: 0.25,
+          }),
+        });
+
+        marker.setStyle(iconStyle);
 
         newSource = new VectorSource({
           features: [marker],
