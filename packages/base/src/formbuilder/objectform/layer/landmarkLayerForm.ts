d3c15f230bac9d202f9f44e45dcae918734b18c7
Greg <gregory.mooney@quantstack.net>
2025-11-25 18:11:41 +0100

    Add form button to update extent and zoom of landmark

--- packages/base/src/formbuilder/objectform/layer/landmarkLayerForm.ts
+++ packages/base/src/formbuilder/objectform/layer/landmarkLayerForm.ts
@@ -1,10 +1,10 @@
 import { IDict } from '@jupytergis/schema';
+import { FieldProps } from '@rjsf/core';
+import * as React from 'react';
 
 import { LayerPropertiesForm } from './layerform';
+import LandmarkReset from '../components/LandmarkReset';
 
-/**
- * The form to modify a hillshade layer.
- */
 export class LandmarkLayerPropertiesForm extends LayerPropertiesForm {
   protected processSchema(
     data: IDict<any> | undefined,
@@ -12,6 +12,34 @@ export class LandmarkLayerPropertiesForm extends LayerPropertiesForm {
     uiSchema: IDict,
   ) {
     super.processSchema(data, schema, uiSchema);
+
+    if (!this.props.model.selected) {
+      return;
+    }
+
+    let layerId: string | undefined = undefined;
+    const selectedKeys = Object.keys(this.props.model.selected);
+
+    // Find the first selected landmark
+    // TODO ! we still need to handle selections better, like there should at least be a getFirstSelected
+    // ! just do that
+    for (const key of selectedKeys) {
+      const layer = this.props.model.getLayer(key);
+      if (layer && layer.type === 'LandmarkLayer') {
+        layerId = key;
+        break;
+      }
+    }
+
+    uiSchema['extent'] = {
+      'ui:field': (props: FieldProps) =>
+        React.createElement(LandmarkReset, {
+          ...props,
+          model: this.props.model,
+          layerId,
+        }),
+    };
+
     uiSchema['content'] = {
       ...uiSchema['content'],
       markdown: {
@@ -21,5 +49,7 @@ export class LandmarkLayerPropertiesForm extends LayerPropertiesForm {
         },
       },
     };
+
+    this.removeFormEntry('zoom', data, schema, uiSchema);
   }
 }
