219469e9f605240e16896bc3511f8aef62c48eb8
Greg <gregory.mooney@quantstack.net>
2025-11-25 18:11:41 +0100

    Add form button to update extent and zoom of landmark

---
+++ packages/base/src/formbuilder/objectform/components/LandmarkReset.tsx
@@ -0,0 +1,52 @@
+import { IJupyterGISModel } from '@jupytergis/schema';
+import { LabIcon } from '@jupyterlab/ui-components';
+import React from 'react';
+
+import { targetWithCenterIcon } from '@/src/icons';
+import { Button } from '@/src/shared/components/Button';
+
+interface ILandmarkResetProps {
+  model?: IJupyterGISModel;
+  layerId?: string;
+}
+
+function LandmarkReset({ model, layerId }: ILandmarkResetProps) {
+  const handleSetLandmarkToCurrentView = () => {
+    if (!model || !layerId) {
+      return;
+    }
+    const layer = model.getLayer(layerId);
+    if (!layer) {
+      return;
+    }
+    const { zoom, extent } = model.getOptions();
+    const updatedLayer = {
+      ...layer,
+      parameters: {
+        ...layer.parameters,
+        zoom,
+        extent,
+      },
+    };
+
+    model.sharedModel.updateLayer(layerId, updatedLayer);
+  };
+
+  return (
+    <div>
+      <Button
+        title="Set landmark to current view"
+        onClick={handleSetLandmarkToCurrentView}
+      >
+        <LabIcon.resolveReact
+          icon={targetWithCenterIcon}
+          className="jp-gis-layerIcon"
+          tag="span"
+        />
+        Reset Landmark Extent
+      </Button>
+    </div>
+  );
+}
+
+export default LandmarkReset;
