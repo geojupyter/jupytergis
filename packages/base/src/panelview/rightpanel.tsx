5d8e483c737737000b9782551dc12672778f7552
Greg Mooney <gregory.mooney@quantstack.net>
2026-01-05 17:10:58 +0100

    Add Story mode presentation toggle button to toolbar

--- packages/base/src/panelview/rightpanel.tsx
+++ packages/base/src/panelview/rightpanel.tsx
@@ -49,9 +49,9 @@ export const RightPanel: React.FC<IRightPanelProps> = props => {
       : false,
     !settings.storyMapsDisabled
       ? {
-          name: 'storyPanel',
-          title: storyPanelTitle,
-        }
+        name: 'storyPanel',
+        title: displayEditor ? 'Story Editor' : 'Story Map',
+      }
       : false,
     !settings.annotationsDisabled
       ? { name: 'annotations', title: 'Annotations' }
