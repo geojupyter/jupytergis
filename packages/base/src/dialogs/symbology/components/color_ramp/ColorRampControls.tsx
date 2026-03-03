63a35f50961153270977a79fcfc094a082ce50d9
Nakul <nakulverma.py@gmail.com>
2026-03-03 10:02:39 +0530

    Suggestions

--- packages/base/src/dialogs/symbology/components/color_ramp/ColorRampControls.tsx
+++ packages/base/src/dialogs/symbology/components/color_ramp/ColorRampControls.tsx
@@ -64,12 +64,18 @@ const ColorRampControls: React.FC<IColorRampControlsProps> = ({
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [reverseRamp, setReverseRamp] = useState<boolean>(false);
   const [warning, setWarning] = useState<string | null>(null);
+  const symbologyState = layerParams.symbologyState;
 
   useEffect(() => {
-    if (layerParams.symbologyState) {
+    if (symbologyState) {
       populateOptions();
     }
-  }, [layerParams.symbologyState]);
+  }, [
+    symbologyState.colorRamp,
+    symbologyState.nClasses,
+    symbologyState.mode,
+    symbologyState.reverseRamp,
+  ]);
 
   useEffect(() => {
     if (!selectedRamp) {
@@ -103,8 +109,7 @@ const ColorRampControls: React.FC<IColorRampControlsProps> = ({
     }
   }, [selectedRamp, numberOfShades]);
   const populateOptions = () => {
-    const { nClasses, mode, colorRamp, reverseRamp } =
-      layerParams.symbologyState ?? {};
+    const { nClasses, mode, colorRamp, reverseRamp } = symbologyState ?? {};
     setNumberOfShades(Number(nClasses ?? 9));
     setSelectedMode((mode as ClassificationMode) ?? 'equal interval');
     setSelectedRamp((colorRamp as ColorRampName) ?? 'viridis');
