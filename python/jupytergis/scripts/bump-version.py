6b96688a96f62c946243822744a6049d5f5980ce
martinRenou <martin.renou@gmail.com>
2026-04-03 10:53:32 +0200

    Use jlpm instead of yarn for bump version

--- python/jupytergis/scripts/bump-version.py
+++ python/jupytergis/scripts/bump-version.py
@@ -100,7 +100,7 @@ def bump():
     root_json = json.loads(package_json.read_text(encoding="utf-8"))
     root_json["version"] = js_version
     package_json.write_text(json.dumps(root_json), encoding="utf-8")
-    run(["yarn", "install"], check=True)
+    run(["jlpm", "install"], check=True)
     run(
         [
             "node",
@@ -117,7 +117,7 @@ def bump():
     # update CITATION.cff metadata
     bump_citation_cff(py_version)
     # bump the JS version with lerna
-    run(f"yarn run bump:js:version {js_version}", shell=True, check=True)
+    run(f"jlpm run bump:js:version {js_version}", shell=True, check=True)
 
 
 if __name__ == "__main__":
