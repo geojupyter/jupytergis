4a0a54ac599555309f516983ccc149b2d17ab8f7
Greg Mooney <gregory.mooney@quantstack.net>
2026-04-13 13:51:10 +0200

    Temp import to get jupyter-tiler dev install to work

--- python/jupytergis/jupytergis/__init__.py
+++ python/jupytergis/jupytergis/__init__.py
@@ -1,3 +1,8 @@
+# This is to make dev jgis work with dev jgis-tiler -- remove eventually
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
 __version__ = "0.14.0"
 
 from jupytergis_lab import GISDocument, explore  # noqa
