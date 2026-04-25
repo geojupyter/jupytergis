1f0b9d33ee10efb3e1d75e6b373e56897877809b
Benjamin Szeghy <benjamin.szeghy@berkeley.edu>
2026-04-24 21:06:25 -0700

    left a few things behind... woops

--- packages/base/src/processingLibrary/buffer.tsx
+++ packages/base/src/processingLibrary/buffer.tsx
@@ -1,4 +1,13 @@
 export default {
-  label: 'Buffer',
-  execute: () => {},
+    label: 'Buffer',
+    form: ()=>(
+        <div>
+            <label htmlFor="distance">Distance</label>
+            <div>
+                <input id="distance" type="number" />
+                <input id="unit" type="text" placeholder="Unit" />
+            </div>
+        </div>
+    ),
+    execute: () => {},
 };
