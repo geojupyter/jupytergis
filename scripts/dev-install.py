a5080defe3ac92dfaa7b65024cd8f6460b1e4187
Matt Fisher <mfisher87@gmail.com>
2026-05-15 16:52:29 -0600

    Fixup dev install script
    As previously-written, pip would install jupytergis_lab from pypi
    because the local install didn't include the tiler extra.

--- scripts/dev-install.py
+++ scripts/dev-install.py
@@ -19,10 +19,16 @@ def install_dev():
     execute("python -m pip install --group build")
     execute("jlpm install --immutable")  # Use lockfile for safety!
     execute("jlpm build")
+
     for py_package in python_packages:
         execute(f"pip uninstall {py_package} -y")
         execute("jlpm clean:all", cwd=root_path / "python" / py_package)
-        execute(f"pip install -e {python_package_prefix}/{py_package}")
+
+        install_cmd = f"pip install -e {python_package_prefix}/{py_package}"
+        if py_package == "jupytergis_lab":
+            install_cmd += "[tiler]"
+
+        execute(install_cmd)
 
         if py_package == "jupytergis_qgis":
             execute("jupyter server extension enable jupytergis_qgis")
