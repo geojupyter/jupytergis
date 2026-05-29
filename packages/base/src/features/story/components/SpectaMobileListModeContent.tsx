d1b50ebf43315172d1fa7d14c447bca762bffa92
Greg Mooney <gregory.mooney@quantstack.net>
2026-05-29 13:42:16 +0200

    Set up mobile list view

---
+++ packages/base/src/features/story/components/SpectaMobileListModeContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/** Placeholder for mobile list-story scroll track (desktop: ListStoryVirtualScrollTrack). */
+export function SpectaMobileListModeContent(): JSX.Element {
+  return (
+    <div
+      id="jgis-story-segment-panel"
+      className="jgis-story-viewer-panel jgis-story-viewer-panel-specta-mod-list jgis-story-mobile-list-mode-stub"
+    >
+      <p>List story mobile view (TODO)</p>
+    </div>
+  );
+}
