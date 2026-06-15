1dc532e5a727faf0474bad966f48bddcdc7a0c9e
Matt Fisher <mfisher87@gmail.com>
2026-06-15 09:52:42 -0600

    Stylize colormap with a space like the glossary
    I think we should probably stylize it like matplotlib does (no space),
    but it's most important to be consistent with the glossary

--- packages/base/src/features/layers/symbology/components/ScaleEditor.tsx
+++ packages/base/src/features/layers/symbology/components/ScaleEditor.tsx
@@ -189,7 +189,7 @@ export const ColorRampEditor: React.FC<IColorRampEditorProps> = ({
   return (
     <div className="jp-gis-color-ramp-container">
       <div className="jp-gis-symbology-row">
-        <label>Colormap</label>
+        <label>Color map</label>
         <ColorRampSelector
           selectedRamp={params.name as ColorRampName}
           setSelected={name => update({ name })}
