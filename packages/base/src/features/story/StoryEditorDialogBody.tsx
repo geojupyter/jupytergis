da329da0a824458852a354f2ce5e08ea3d4fb991
Greg Mooney <gregory.mooney@quantstack.net>
2026-06-25 14:37:20 +0200

    Make segments reorderable

--- packages/base/src/features/story/StoryEditorDialogBody.tsx
+++ packages/base/src/features/story/StoryEditorDialogBody.tsx
@@ -259,6 +259,7 @@ export function StoryEditorDialogBody({
     addSegment,
     removeSegment,
     canRemoveSegment,
+    reorderSegments,
     updateStory,
     updateSegmentContentMode,
     updateSegmentContent,
@@ -283,6 +284,7 @@ export function StoryEditorDialogBody({
           selectedSegmentId={selectedSegmentId}
           onSelectSegment={selectSegment}
           onAddSegment={addSegment}
+          onReorderSegments={reorderSegments}
         />
 
         <main className="jgis-story-editor-workspace">
