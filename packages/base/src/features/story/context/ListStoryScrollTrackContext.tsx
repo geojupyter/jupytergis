ea45e41462c70e4ddfb5ee609ec13816c1d3788b
Greg Mooney <gregory.mooney@quantstack.net>
2026-07-28 14:21:27 +0200

    Set up css var parse

--- packages/base/src/features/story/context/ListStoryScrollTrackContext.tsx
+++ packages/base/src/features/story/context/ListStoryScrollTrackContext.tsx
@@ -98,6 +98,7 @@ export function ListStoryScrollTrackProvider({
       storyData?.presentationBgColor,
       storyData?.presentationTextColor,
       storyData?.overlayContentWidth,
+      storyData?.markdownSegmentOpacity,
     ],
   );
 
