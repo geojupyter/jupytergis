324b173cec65e37cae5d5aa5ba862cf484052342
Greg Mooney <gregory.mooney@quantstack.net>
2026-07-28 16:43:39 +0200

    Reorder sections

--- packages/base/src/features/story/components/StoryEditorHeaderBar.tsx
+++ packages/base/src/features/story/components/StoryEditorHeaderBar.tsx
@@ -302,6 +302,12 @@ function StorySettingsPopover({
                     }}
                   />
                 </label>
+                <OverlayContentWidthField
+                  value={story.overlayContentWidth}
+                  onChange={overlayContentWidth => {
+                    onUpdateStory({ overlayContentWidth });
+                  }}
+                />
                 <StoryOpacityField
                   label="Markdown segment opacity"
                   value={story.markdownSegmentOpacity}
@@ -309,21 +315,15 @@ function StorySettingsPopover({
                     onUpdateStory({ markdownSegmentOpacity });
                   }}
                 />
-                <StoryOpacityField
-                  label="Map overlay opacity"
-                  value={story.mapOverlayOpacity}
-                  onChange={mapOverlayOpacity => {
-                    onUpdateStory({ mapOverlayOpacity });
-                  }}
-                />
-                <OverlayContentWidthField
-                  value={story.overlayContentWidth}
-                  onChange={overlayContentWidth => {
-                    onUpdateStory({ overlayContentWidth });
-                  }}
-                />
               </>
             ) : null}
+            <StoryOpacityField
+              label="Map overlay opacity"
+              value={story.mapOverlayOpacity}
+              onChange={mapOverlayOpacity => {
+                onUpdateStory({ mapOverlayOpacity });
+              }}
+            />
             <label className="jgis-story-editor-field">
               <span>Background color</span>
               <Input
