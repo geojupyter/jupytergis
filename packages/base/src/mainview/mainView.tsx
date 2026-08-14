759ad5819c98044ef451ea7dbe174c811bd00dff
Benjamin Szeghy <benjamin.szeghy@berkeley.edu>
2026-08-13 21:15:45 -0700

    add todo comment to fix a weird bug

--- packages/base/src/mainview/mainView.tsx
+++ packages/base/src/mainview/mainView.tsx
@@ -4406,6 +4406,11 @@ function MainViewWithObserver(
   );
 
   React.useEffect(() => {
+    /*TODO (fix): this creates a rare bug where certain togglable features 
+    (like the location indicator and draw tool) behave unexpectedly following a projection 
+    change. this is causes by the fact that the mainView component is unmounted and remounted on projection change,
+    but the toolbar state is not reset.
+    */
     const model = props.viewModel.jGISModel;
     const onChange = () => {
       const projection = model.getOptions().projection;
