7e30a8d5cb4f447e33998e2873c273ab1a13b2ff
arjxn-py <arjxn.py@gmail.com>
2026-08-28 16:51:50 +0530

    Import proj-codes through its entry point in jest

--- packages/jest.base.js
+++ packages/jest.base.js
@@ -6,15 +6,10 @@ module.exports = {
   moduleNameMapper: {
     // Resolve @/* path alias (tsconfig: "@/*" -> "./*" relative to package root)
     '^@/(.*)$': '<rootDir>/$1',
-    // proj-codes' entry point is ESM and imports its data with a JSON import
-    // attribute, which jest cannot parse. The data is the whole package, so
-    // point at it directly: the default export is the same object either way.
-    '^proj-codes$':
-      '<rootDir>/../../node_modules/proj-codes/dist/_generated/proj-codes.json',
   },
   transform: {
     '^.+\\.(tsx?|js)$': ['ts-jest', { tsconfig: './tsconfig.test.json' }],
   },
   // Transform ESM-only packages that have no CommonJS build
-  transformIgnorePatterns: ['/node_modules/(?!(py2vega-ts|vega2ol|vega-.*|d3-.*|@jupyter/ydoc|ol)/)'],
+  transformIgnorePatterns: ['/node_modules/(?!(py2vega-ts|proj-codes|vega2ol|vega-.*|d3-.*|@jupyter/ydoc|ol)/)'],
 };
