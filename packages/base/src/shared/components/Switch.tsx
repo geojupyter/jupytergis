2416f941af17b549480036406d34de519a2018d7
Greg Mooney <gregory.mooney@quantstack.net>
2026-09-01 17:18:47 +0200

    Switch

--- packages/base/src/shared/components/Switch.tsx
+++ packages/base/src/shared/components/Switch.tsx
@@ -1,21 +1,28 @@
-import { Switch as SwitchPrimitive } from 'radix-ui';
+import { Switch as SwitchPrimitive } from '@base-ui/react/switch';
 import * as React from 'react';
 
 import { cn } from './utils';
 
 function Switch({
   className,
+  size = 'default',
   ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+}: SwitchPrimitive.Root.Props & {
+  size?: 'sm' | 'default';
+}) {
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
-      className={cn('jgis-switch', className)}
+      data-size={size}
+      className={cn(
+        'peer group/switch relative inline-flex shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none group-has-[:focus-visible]/field-label:border-transparent group-has-[:focus-visible]/field-label:ring-0 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 data-checked:bg-primary data-disabled:cursor-not-allowed data-disabled:opacity-50 data-unchecked:bg-input data-[size=default]:h-[18.4px] data-[size=default]:w-[32px] data-[size=sm]:h-[14px] data-[size=sm]:w-[24px]',
+        className,
+      )}
       {...props}
     >
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
-        className="jgis-switch-thumb"
+        className="pointer-events-none block rounded-full bg-background ring-0 transition-transform group-data-[size=default]/switch:size-4 group-data-[size=sm]/switch:size-3 group-data-[size=default]/switch:data-checked:translate-x-[calc(100%-2px)] group-data-[size=sm]/switch:data-checked:translate-x-[calc(100%-2px)] group-data-[size=default]/switch:data-unchecked:translate-x-0 group-data-[size=sm]/switch:data-unchecked:translate-x-0"
       />
     </SwitchPrimitive.Root>
   );
