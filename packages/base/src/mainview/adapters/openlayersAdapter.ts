66ee9d16e207282cd9001a1c5f7840893452fdd7
Nakul <nakulverma.py@gmail.com>
2026-09-14 16:19:29 +0530

    update code after resolving conflict

--- packages/base/src/mainview/adapters/openlayersAdapter.ts
+++ packages/base/src/mainview/adapters/openlayersAdapter.ts
@@ -109,8 +109,10 @@ import projcodes from 'proj-codes';
 import proj4 from 'proj4';
 
 import { DrawToolController } from '@/src/features/draw-tool';
-import { ensureHighlightLayer } from '@/src/features/identify/utils/highlightLayer';
-import { buildHighlightStyle } from '@/src/features/identify/utils/highlightStyle';
+import {
+  ensureHighlightLayer,
+  buildHighlightStyle,
+} from '@/src/features/identify/utils/highlightLayer';
 import {
   OpenEOTileLayer,
   OpenEOTileSource,
@@ -451,7 +453,11 @@ export class OpenLayersAdapter implements IMapAdapter {
     const selectedLayer = localState?.selected?.value;
 
     if (!selectedLayer) {
-      this._log('warning', 'Layer must be selected to use identify tool');
+      if (this._model.isStoryPresentationActive()) {
+        this._identifyFeaturesAtPixel(e);
+      } else {
+        this._log('warning', 'Layer must be selected to use identify tool');
+      }
       return;
     }
 
@@ -463,72 +469,9 @@ export class OpenLayersAdapter implements IMapAdapter {
         // Handled by selectInteraction (createSelectInteraction).
         break;
 
-      case 'VectorTileLayer': {
-        const geometries: OLGeometry[] = [];
-        const features: IIdentifiedFeatureEntry[] = [];
-        let foundAnyFeatures = false;
-
-        this._map.forEachFeatureAtPixel(e.pixel, (feature: FeatureLike) => {
-          foundAnyFeatures = true;
-
-          let geom: OLGeometry | undefined;
-          let props = {};
-
-          if (feature instanceof RenderFeature) {
-            geom = toGeometry(feature);
-          } else if ('getGeometry' in feature) {
-            geom = feature.getGeometry();
-          }
-
-          const rawProps = feature.getProperties();
-          const fid = feature.getId?.() ?? rawProps?.fid;
-
-          if (rawProps && Object.keys(rawProps).length > 1) {
-            const { ...clean } = rawProps;
-            props = clean;
-            if (fid !== null) {
-              // TODO Clean the cache under some condition?
-              this._featureAttributeCache.set(fid, props);
-            }
-          } else if (fid !== null && this._featureAttributeCache.has(fid)) {
-            props = this._featureAttributeCache.get(fid);
-          }
-
-          if (geom) {
-            geometries.push(geom);
-          }
-          if (props && Object.keys(props).length > 0) {
-            features.push({
-              feature: props,
-              floaterOpen: false,
-            });
-          }
-
-          return true;
-        });
-
-        if (features.length > 0) {
-          this._model.syncIdentifiedFeatures(
-            features,
-            this._model.getClientId().toString(),
-          );
-        } else if (!foundAnyFeatures) {
-          this._model.syncIdentifiedFeatures(
-            [],
-            this._model.getClientId().toString(),
-          );
-        }
-
-        if (geometries.length > 0) {
-          for (const geom of geometries) {
-            this._model.highlightFeatureSignal.emit(geom);
-          }
-        } else {
-          const coordinate = this._map.getCoordinateFromPixel(e.pixel);
-          const point = new Point(coordinate);
-          this._model.highlightFeatureSignal.emit(point);
-        }
-
+      case 'VectorTileLayer':
+      case 'StorySegmentLayer': {
+        this._identifyFeaturesAtPixel(e);
         break;
       }
 
@@ -553,7 +496,12 @@ export class OpenLayersAdapter implements IMapAdapter {
         bandValues['Alpha'] = data[data.length - 1];
 
         this._model.syncIdentifiedFeatures(
-          [{ feature: bandValues, floaterOpen: false }],
+          [
+            {
+              feature: bandValues,
+              floaterOpen: this._model.isStoryPresentationActive(),
+            },
+          ],
           this._mainViewId ?? this._model.getClientId().toString(),
         );
 
@@ -565,6 +513,74 @@ export class OpenLayersAdapter implements IMapAdapter {
 
         break;
       }
+    }
+  }
+
+  private _identifyFeaturesAtPixel(e: MapBrowserEvent<any>): void {
+    const geometries: OLGeometry[] = [];
+    const features: IIdentifiedFeatureEntry[] = [];
+    let foundAnyFeatures = false;
+
+    this._map.forEachFeatureAtPixel(e.pixel, (feature: FeatureLike) => {
+      foundAnyFeatures = true;
+
+      let geom: OLGeometry | undefined;
+      let props = {};
+
+      if (feature instanceof RenderFeature) {
+        geom = toGeometry(feature);
+      } else if ('getGeometry' in feature) {
+        geom = feature.getGeometry();
+      }
+
+      const rawProps = feature.getProperties();
+      const fid = feature.getId?.() ?? rawProps?.fid;
+
+      if (rawProps && Object.keys(rawProps).length > 1) {
+        const { ...clean } = rawProps;
+        props = clean;
+        if (fid !== null) {
+          // TODO Clean the cache under some condition?
+          this._featureAttributeCache.set(fid, props);
+        }
+      } else if (fid !== null && this._featureAttributeCache.has(fid)) {
+        props = this._featureAttributeCache.get(fid);
+      }
+
+      if (geom) {
+        geometries.push(geom);
+      }
+      if (props && Object.keys(props).length > 0) {
+        features.push({
+          feature: props,
+          // Specta has no IdentifyPanel to open floaters so we show them on identify.
+          floaterOpen: this._model.isStoryPresentationActive(),
+        });
+      }
+
+      return true;
+    });
+
+    if (features.length > 0) {
+      this._model.syncIdentifiedFeatures(
+        features,
+        this._model.getClientId().toString(),
+      );
+    } else if (!foundAnyFeatures) {
+      this._model.syncIdentifiedFeatures(
+        [],
+        this._model.getClientId().toString(),
+      );
+    }
+
+    if (geometries.length > 0) {
+      for (const geom of geometries) {
+        this._model.highlightFeatureSignal.emit(geom);
+      }
+    } else {
+      const coordinate = this._map.getCoordinateFromPixel(e.pixel);
+      const point = new Point(coordinate);
+      this._model.highlightFeatureSignal.emit(point);
     }
   }
 
@@ -839,6 +855,7 @@ export class OpenLayersAdapter implements IMapAdapter {
 
     const interactionArray = this._map.getInteractions().getArray();
 
+    // Remove interactions except Select (used by identify)
     const interactionsToRemove = [
       DragPan,
       DragRotate,
@@ -1284,7 +1301,8 @@ export class OpenLayersAdapter implements IMapAdapter {
       selectInteraction.getFeatures().forEach(feature => {
         identifiedFeatures.push({
           feature: feature.getProperties(),
-          floaterOpen: false,
+          // Specta has no IdentifyPanel to open floaters so we show them on identify.
+          floaterOpen: this._model.isStoryPresentationActive(),
         });
         const geom = feature.getGeometry();
         if (geom) {
