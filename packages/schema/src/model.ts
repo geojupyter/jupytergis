1322b6107033cf4f7d299ff10d5bd5cf65331c2d
arjxn-py <arjxn.py@gmail.com>
2026-09-15 13:51:02 +0530

    fix test

--- packages/schema/src/model.ts
+++ packages/schema/src/model.ts
@@ -88,8 +88,10 @@ export class JupyterGISModel implements IJupyterGISModel {
       this._sharedModel.changed.connect(this._onSharedModelChanged);
     }
     this.sharedModel.awareness.on('change', this._onClientStateChanged);
-    window.addEventListener('beforeunload', this._onPageExit);
-    window.addEventListener('pagehide', this._onPageExit);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('beforeunload', this._onPageExit);
+      window.addEventListener('pagehide', this._onPageExit);
+    }
     this._sharedModel.metadataChanged.connect(
       this._metadataChangedHandler,
       this,
@@ -396,8 +398,10 @@ export class JupyterGISModel implements IJupyterGISModel {
       return;
     }
     this._isDisposed = true;
-    window.removeEventListener('beforeunload', this._onPageExit);
-    window.removeEventListener('pagehide', this._onPageExit);
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('beforeunload', this._onPageExit);
+      window.removeEventListener('pagehide', this._onPageExit);
+    }
     this._sharedModel.dispose();
     this._disposed.emit();
     Signal.clearData(this);
